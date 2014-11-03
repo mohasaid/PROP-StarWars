fe57d4c9e82b793e5775f47751632e58583da784
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-03 14:08:04 +0100

    Create Arco.java

---
+++ Arco.java
@@ -0,0 +1,87 @@
+import java.util.*;
+ 
+public class Arco {
+    private int id;
+    private Nodo origen;
+    private Nodo destino;
+    private int capacidad;
+    private int coste; //Contiene la distancia/precio del arco según la función de coste escogida
+ 
+     
+//CREADORAS
+     
+    //Pre: cierto
+    //Post: crea un arco vacío
+    public void Arco(){
+        id = -1;
+        origen = new Nodo();
+        destino = new Nodo();
+        capacidad = 0;
+        coste = -1;
+    }
+    //Pre:cierto
+    //Post: Crea un Arco inicializado
+    public void Arco(int ident, Nodo o, Nodo d, int c){
+        id = ident;
+        origen = o;
+        destino = d;
+        capacidad = c;
+        coste = -1;
+    }
+ 
+//CONSULTORAS
+     
+    //Pre: Cierto
+    //Post: Devuelve el identificador del arco
+    public int ConsultarId(){
+        return id;
+    }
+    //Pre: Cierto
+    //Post: Devuelve el nodo origen del arco
+    public Nodo ConsultarOrigen(){
+        return origen;
+    }
+    //Pre: Cierto
+    //Post: Devuelve el nodo oestino del arco
+    public Nodo ConsultarDestino(){
+        return destino;
+    }
+    //Pre: Cierto
+    //Post: Devuelve la capacidad del arco
+    public int ConsultarCapacidad(){
+        return capacidad;
+    }
+    //Pre: Cierto
+    //Post: Devuelve el coste del arco
+    public int ConsultarCoste(){
+        return coste;
+    }
+     
+//MODIFICADORAS
+     
+    //Pre: Cierto
+    //Post: el identificador del arco pasa a ser idn
+    public void ModificarId(int idn){
+        id = idn;
+    }
+    //Pre:Cierto
+    //Post: el nodo origen del arco pasa a ser on
+    public void ModificarOrigen(Nodo on){
+        origen = on;
+    }
+    //Pre: Cierto
+    //Post: el nodo destino del arco pasa a ser dn
+    public void ModificarDestino(Nodo dn){
+        destino = dn;
+    }
+    //Pre: Cierto
+    //Post: la capacidad del arco pasa a valer cn
+    public void ModificarCapacidad(int cn){
+        capacidad = cn;
+    }
+    //Pre: Cierto
+    //Post el coste del arco pasa valer cn
+    public void ModificarCoste(int cn){
+        coste = cn;
+    }
+}
