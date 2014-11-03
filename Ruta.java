a31ce573b2077a3170c374280f1fbde3247fae53
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-03 13:24:21 +0100

    Create Ruta.java

---
+++ Ruta.java
@@ -0,0 +1,153 @@
+import java.util.Comparator;
+
+public class Ruta {
+    //PARTE PRIVADA
+    private int id;
+    private int capacidad;
+    private int distancia;
+    private int planetaA;
+    private int planetaB;
+    private boolean bidireccional;
+        
+    private static boolean ErrorTipografico(int i) {
+        return (i<0);
+    }
+    
+    //PARTE PUBLICA
+    
+    //CONSTRUCTORAS
+    public Ruta()
+    {
+        id = -1;
+        capacidad = -1;
+        distancia = -1;
+        planetaA = -1;
+        planetaB = -1;
+        bidireccional = false; //Por defecto una ruta es dirigida, no bidireccional
+    }
+        
+        
+    public Ruta(int id, int capacidad, int distancia, int planetaA, int planetaB, boolean bidireccional) throws Exception
+    {
+        if(ErrorTipografico(id)){
+            throw new Exception("Error : El identificador de una ruta debe ser mayor o igual que 0");
+        }
+        if(ErrorTipografico(capacidad)){
+            throw new Exception("Error : La capacidad debe ser mayor o igual que 0");
+        }
+        if(ErrorTipografico(distancia) || distancia == 0){
+            throw new Exception("Error : La distancia entre planeas debe ser mayor que 0");
+        }
+        if(ErrorTipografico(planetaA) || ErrorTipografico(planetaB)){
+            throw new Exception("Error : El identificador de un planeta debe ser mayor o igual que 0");
+        }
+        this.id = id;
+        this.capacidad = capacidad;
+        this.distancia = distancia;
+        this.planetaA = planetaA;
+        this.planetaB = planetaB;
+        this.bidireccional = bidireccional;
+    }
+             
+    //CONSULTORAS
+        
+    public int consultar_id() throws Exception
+    {
+        if(id == -1) throw new Exception("Error: La ruta no tiene un id asignado");
+        return id;
+    }
+        
+    public int consultar_capacidad() throws Exception
+    {
+        if(capacidad == -1) throw new Exception("Error: La ruta no tiene una capacidad asignada");
+        return capacidad;
+    }
+        
+        
+    public int consultar_distancia() throws Exception
+    {
+        if(distancia == -1) throw new Exception("Error: La ruta no tiene una asignado");
+        return distancia;
+    }
+        
+        
+    public int consultar_planetaA() throws Exception
+    {
+        if(planetaA == -1) throw new Exception("Error: La ruta no tiene un planeta origen asignado");
+        return planetaA;
+    }
+        
+        
+    public int consultar_planetaB() throws Exception
+    {
+        if(planetaB == -1) throw new Exception("Error: La ruta no tiene un planeta destino asignado");
+        return planetaB;
+    }
+        
+        
+    public boolean consultar_bidireccional() throws Exception
+    {
+        return bidireccional;
+    }
+        
+    //MODIFICDORAS
+        
+    public void modificar_id(int id_nuevo) throws Exception
+    {
+        if(ErrorTipografico(id_nuevo)){
+            throw new Exception("Error : El identificador de una ruta debe ser mayor o igual que 0");
+        }
+        id = id_nuevo;
+    }
+        
+    public void modificar_capacidad(int k) throws Exception
+    {
+        if(ErrorTipografico(capacidad)){
+            throw new Exception("Error : La capacidad debe ser mayor o igual que 0");
+        }
+        capacidad = k;
+    }
+        
+        
+    public void modificar_distancia(int d) throws Exception
+    {
+        if(ErrorTipografico(distancia) || distancia == 0){
+            throw new Exception("Error : La distancia entre planeas debe ser mayor que 0");
+        }
+        distancia = d;
+    }
+        
+        
+    public void modificar_planetaA(int idA) throws Exception
+    {
+        if(ErrorTipografico(planetaA)) throw new Exception("Error : El identificador de un planeta debe ser mayor o igual que 0");
+        planetaA = idA;
+    }
+        
+        
+    public void modificar_planetaB(int idB) throws Exception
+    {
+        if(ErrorTipografico(planetaB)) throw new Exception("Error : El identificador de un planeta debe ser mayor o igual que 0");
+        planetaB = idB;
+    }
+        
+        
+    public void modificar_bidireccional(boolean b) throws Exception
+    {
+        bidireccional = b;
+    } 
+   
+}
+
+class OrdenRuta implements Comparator<Ruta>{
+    public int compare(Ruta r1, Ruta r2) { 
+        try { 
+            if(r2.consultar_id() < r1.consultar_id()) return 1; 
+            else return -1; 
+            } 
+        catch (Exception e) { 
+            System.out.print(e); 
+            } 
+        return 0; 
+    } 
+}
