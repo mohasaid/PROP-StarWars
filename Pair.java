b11387b6195083301eb0a7f432111aca4103f4a8
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-08 22:44:59 +0100

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -2,13 +2,13 @@ public class Pair<L,R> {
 	private L first; // Left
     private R second; // Right
  
-    public Pair(L first, R second) {
-        this.first = first;
-        this.second = second;
+    public Pair(L first1, R second1) {
+        first = first1;
+        second = second1;
     }
  
-    public L getFirst() { return first;}
-    public R getSecond() { return second;}
-    public void setFirst(L first) { this.first = first;}
-    public void setSecond(R second) { this.second = second;}
+    public L consultarPrimero() { return first;}
+    public R consultarSegundo() { return second;}
+    public void ponPrimero(L first1) { first = first1;}
+    public void ponSegundo(R second1) { second = second1;}
 }
