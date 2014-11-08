8b91de564678da9272fdd38685d5e5df3841e1b4
Gerard9494 <gerard9494@gmail.com>
2014-11-08 20:31:49 +0100

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,3 +1,4 @@
+
 public class Nodo {
     private int id;
     private boolean Yavin_IV;
@@ -5,31 +6,40 @@ public class Nodo {
      
     //CREADORA
      
-    //Pre:
-    //Post:
+    //Pre: Cierto
+    //Post: Retorna un Nodo vacio
     public Nodo()
     {
         id = -1;
     }
      
-    public Nodo (int id, boolean es_Yavin, boolean es_Hoth)
+    //Pre: Cierto
+    //Post: Retorna un Nodo con id = "ident", Yavin_IV = "es_Yavin" y Hoth = "es_Hoth"
+    public Nodo (int ident, boolean es_Yavin, boolean es_Hoth)
     {
-        this.id = id;
-        this.Yavin_IV = es_Yavin;
-        this.Hoth = es_Hoth;
+        id = ident;
+        Yavin_IV = es_Yavin;
+        Hoth = es_Hoth;
     }
      
     //Consultoras
+    
+    //Pre: Cierto
+    //Post: Retorna un entero que contiene la id del Nodo
     public int consultar_id ()
     {
         return id;
     }
      
+    //Pre: Cierto
+    //Post: Retorna un valor booleano. Si retorna cierto, este Nodo representa el planeta Yavin, en caso contrario, representa cualquier otro planeta
     public boolean consultar_es_Yavin()
     {
         return Yavin_IV;
     }
      
+    //Pre: Cierto
+    //Post: Retorna un valor booleano. Si retorna cierto, este Nodo representa el planeta Hoth, en caso contrario, representa cualquier otro planeta
     public boolean consultar_es_Hoth()
     {
         return Hoth;
