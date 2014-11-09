af8a1b3d942378fad1900dac23466722053e0d36
JoseMartinNogueira <josemn15@hotmail.com>
2014-11-09 18:04:00 +0100

    Practicamente Acabado (falta revisar cosas)

--- DriverControladorPlaneta.java
+++ DriverControladorPlaneta.java
@@ -21,15 +21,20 @@ public class DriverControladorPlaneta {
             + " 10: Consultar las Rutas que salen del Planeta -> public TreeSet<Integer> Consultar_RutasSalida(int id)\n"
             + " 11: Consultar las Rutas que etran al Planeta -> public TreeSet<Integer> Consultar_RutasEntrada(int id)\n"
             + " 12: Consultar las Naves que estan en el Planeta -> public TreeSet<Integer> Consultar_LNaves(int id)\n"
-            + " 13: Modificar id -> public void Modificar_id(Planeta p, int id)\n"
-            + " 14: Modificar Capacidad -> public void Modificar_Capacidad(Planeta p, int c)\n"
-            + " 15: Modificar Coste -> public void Modificar_Coste(Planeta p, int k)\n"
-            + " 16: Modificar Coordenadas -> public void modificarCoordenades(Planeta p, int x, int y)\n"
-            + " 17: Añadir una Ruta que sale del Planeta -> public void Anadir_Salida(Planeta p, int id)\n"
-            + " 18: Añadir una Ruta que entra al Planeta -> public void Anadir_Salida(Planeta p, int id)\n"
-            + " 19: Borrar una Ruta que sale del Planeta -> public void Borrar_Salida(Planeta p, int id)\n"
-            + " 20: Borrar una Ruta que entra al Planeta -> public void Borrar_Entrada(Planeta p, int id)\n"
-            + " 21: Borrar el Planeta -> public void Borrar(Planeta p)");
+            + " 13: Consultar si una id pertenece a un Planeta -> public Planeta BuscarPlaneta(int id)\n"
+            + " 14: Consultar la list de Planetas -> public TreeSet<Planeta> Consultar_listaPlanetas()\n"
+            + " 15: Consultar el size de la listaPlanetas -> public int Consultar_Size()"
+            + " 16: Modificar id -> public void Modificar_id(int idold, int idnew)\n"
+            + " 17: Modificar Capacidad -> public void Modificar_Capacidad(int id, int c)\n"
+            + " 18: Modificar Coste -> public void Modificar_Coste(int id, int k)\n"
+            + " 19: Modificar Coordenadas -> public void modificarCoordenades(int id, int x, int y)\n"
+            + " 20: Anadir una Ruta que sale del Planeta -> public void Anadir_Salida(int idp, int id)\n"
+            + " 21: Anadir una Ruta que entra al Planeta -> public void Anadir_Salida(int idp, int id)\n"
+            + " 22: Anadir una Nave que esta en el Planeta -> public void Anadir_Nave(int idp, int id)\n"
+            + " 23: Borrar una Ruta que sale del Planeta -> public void Borrar_Salida(int idp, int id)\n"
+            + " 24: Borrar una Ruta que entra al Planeta -> public void Borrar_Entrada(int idp, int id)\n"
+            + " 25: Borrar una Nave que estaba en el Planeta -> public void Borrar_Nave(int idp, int id)\n"
+            + " 26: Borrar el Planeta -> public void Borrar(Planeta p)");
         n = cin.nextInt();
         while(n != 0) {
             switch(n) {
@@ -45,15 +50,20 @@ public class DriverControladorPlaneta {
                 case 10: TestConsultarRutasSalida(sc, contp);break;
                 case 11: TestConsultarRutasEntrada(sc, contp);break;
                 case 12: TestConsultarLNaves(sc, contp);break;
-                case 13: TestModificarId(sc, contp);break;
-                case 14: TestModificarCapacidad(sc, contp);break;
-                case 15: TestModificarCoste(sc, contp);break;
-                case 16: TestModificarCoordenadas(sc, contp);break;
-                case 17: TestAnadirSalida(sc, contp);break;
-                case 18: TestAnadirEntrada(sc, contp);break;
-                case 19: TestBorrarSalida(sc, contp);break;
-                case 20: TestBorrarEntrada(sc, contp);break;
-                case 21: TestBorrar(sc, contp);break;
+                case 13: TestBuscarPlaneta(sc, contp);break;
+                case 14: TestConsultarlistaPlanetas(contp);break;
+                case 15: TestConsultarSize(contp);break;
+                case 16: TestModificarId(sc, contp);break;
+                case 17: TestModificarCapacidad(sc, contp);break;
+                case 18: TestModificarCoste(sc, contp);break;
+                case 19: TestModificarCoordenadas(sc, contp);break;
+                case 20: TestAnadirSalida(sc, contp);break;
+                case 21: TestAnadirEntrada(sc, contp);break;
+                case 22: TestAnadirNave(sc, contp);break;
+                case 23: TestBorrarSalida(sc, contp);break;
+                case 24: TestBorrarEntrada(sc, contp);break;
+                case 25: TestBorrarNave(sc, contp);break;
+                case 26: TestBorrar(sc, contp);break;
             }
             n = sc.nextInt();
     }
@@ -245,7 +255,43 @@ public class DriverControladorPlaneta {
             System.out.print(e);
         }          
     }
-    public static void TestModificarId(Scanner sc, ControladrPlaneta contp) {
+    public static void TestBuscarPlaneta(Scanner sc, ControladorPlaneta contp) {
+    	try {
+    		String s;
+    		while (!sc,hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: El identificador de un Planeta es un entero\n")
+    		}
+    		int id = sc.nextInt();
+    		Planeta p = contp.BuscarPlaneta(id);
+    		if (p != null) System.out.print("El Planeta existe");
+    		else System.out.print("El Planeta no existe");
+    	}
+    	catch (Exception e){
+    		Sytem.out.print(e)
+    	}
+    }
+    public static void TestConsultarlistaPlaneta(ControladorPlaneta contp) {
+        try {
+        	TreeSet<Planeta> lp = contp.Consultar_listaPlanetas();
+        	Iterator<Planeta> it = lp.iterator();
+            while (it.hasNext()) {
+                System.out.print(it.next().Consultar_id();           
+            }
+        }
+        catch (Exception e) {
+            System.out.print(e);
+        }          
+    }
+    public static void TestConsultarSize(ControladorPlaneta contp){
+    	try {
+    		System.out.print(contp.Consultar_Size());
+    	}
+    	catch (Exception e) {
+    		System.out.print(e);
+    	}
+    }
+    public static void TestModificarId(Scanner sc, ControladorPlaneta contp) {
         try {
             String s;
             while (!sc.hasNextInt()) {
@@ -258,6 +304,200 @@ public class DriverControladorPlaneta {
                 throw new Exception("Error: El identificador de un Planeta tiene que ser un entero\n");
             }
             int idnew = sc.nextInt();
-            cont
+            cont.Modificar_id(idact,idnew);
         }
+        catch (Exception e){
+        	System.out.print(e);
+        }
+    }
+    public static void TestModificarCapacidad(Scanner sc, ControladorPlaneta contp){
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int id = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: la Capacidad de un Planeta tiene que ser un entero");
+    		}
+    		int c = sc.nextInt();
+    		contp.Modificar_Capacidad(id,c);
+    	}
+    	catch (Exception e) {
+    		System.out.print(e);
+    	}
+    }
+    public static void TestModificarCoste(Scanner sc, ControladorPlaneta contp){
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int id = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el Coste de un Planeta tiene es un entero");
+    		}
+    		int k = sc.nextInt();
+    		contp.Modificar_Coste(id,k);
+    	}
+    	catch (Exception e) {
+    		System.out.print(e);
+    	}
+    }
+    public static void TestModificarCoordenadas(Scanner sc, ControladorPlaneta contp){
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int id = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: la coordenada X de un Planeta tiene que ser un entero");
+    		}
+    		int x = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: la coordenada Y de un Planeta tiene que ser un entero");
+    		}
+    		int y = sc.nextInt();
+    		contp.modificarCoordenadas(id,x,y);
+    	}
+    	catch (Exception e) {
+    		System.out.print(e);
+    	}
+    }
+    public static void TestAnadirSalida(Scanner sc, COntroladorPlaneta contp) {
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int idp = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de una Ruta tiene que ser un entero");
+    		}
+    		int id = sc.nextInt();
+    		contp.Anadir_Salida(idp, id);
+    	}
+    	catch (Exception e){
+    		System.out.print(e);
+    	}
+    }
+    public static void TestAnadirEntrada(Scanner sc, COntroladorPlaneta contp) {
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int idp = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de una Ruta tiene que ser un entero");
+    		}
+    		int id = sc.nextInt();
+    		contp.Anadir_Entrada(idp, id);
+    	}
+    	catch (Exception e){
+    		System.out.print(e);
+    	}
+    }
+    public static void TestAnadirNave(Scanner sc, COntroladorPlaneta contp) {
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int idp = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de una Nave tiene que ser un entero");
+    		}
+    		int id = sc.nextInt();
+    		contp.Anadir_Nave(idp, id);
+    	}
+    	catch (Exception e){
+    		System.out.print(e);
+    	}
+    }
+    public static void TestBorrarSalida(Scanner sc, COntroladorPlaneta contp) {
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int idp = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de una Ruta tiene que ser un entero");
+    		}
+    		int id = sc.nextInt();
+    		contp.Borrar_Salida(idp, id);
+    	}
+    	catch (Exception e){
+    		System.out.print(e);
+    	}
+    }
+    public static void TestBorrarEntrada(Scanner sc, COntroladorPlaneta contp) {
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int idp = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de una Ruta tiene que ser un entero");
+    		}
+    		int id = sc.nextInt();
+    		contp.Borrar_Entrada(idp, id);
+    	}
+    	catch (Exception e){
+    		System.out.print(e);
+    	}
+    }
+    public static void TestBorrarNave(Scanner sc, COntroladorPlaneta contp) {
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int idp = sc.nextInt();
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de una Nave tiene que ser un entero");
+    		}
+    		int id = sc.nextInt();
+    		contp.Borrar_Nave(idp, id);
+    	}
+    	catch (Exception e){
+    		System.out.print(e);
+    	}
+    }
+    public static void TestBorrar(Scanner sc, COntroladorPlaneta contp) {
+    	try {
+    		String s;
+    		while (!sc.hasNextInt()) {
+    			s = sc.nextLine();
+    			throw new Exception("Error: el identificador de un Planeta es un entero");
+    		}
+    		int id = sc.nextInt();
+    		contp.Borrar(id);
+    	}
+    	catch (Exception e){
+    		System.out.print(e);
+    	}
+    }
 }
