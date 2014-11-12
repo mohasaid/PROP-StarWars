2acb3ee640f1c2944d8b01fbd5a95ce3de5dda7e
Gerard9494 <gerard9494@gmail.com>
2014-11-12 18:28:09 +0100

    Update Entrada.java

--- Entrada.java
+++ Entrada.java
@@ -4,19 +4,21 @@ public class Entrada {
 	private Grafo g;
 	
     //Pre: Cierto
-    //Post: Retorna una entrada vacia
+    //Post: Crea una entrada vacia
     public Entrada()
     {
     	g = new Grafo();
     }
     
     //Pre: Cierto
-    //Post: 
-    public Entrada(Grafo aux)
+    //Post: Crea una entrada con el grafo inicializado a partir de de la lista de nodos y otra de arcos
+    public Entrada(ArrayList<Nodo> nodos, ArrayList<Arco> arcos)
     {
-    	g = new Grafo(aux.getNodes(), aux.getEdges());
+    	g = new Grafo(nodos, arcos);
     }
     
+    
+    /*                       ******************EN PRINCIPIO ESO LO TIENE MOHA*************************
     //Pre: Cierto
     //Post: Retorna un nodo que representa el planeta p	
 	public Nodo transformar_PlanetaaNodo(Planeta p)
@@ -26,7 +28,7 @@ public class Entrada {
 	}
 	
     //Pre: El planeta p existe en la galaxia
-    //Post: El nodo n, que representa el planeta p, se ha añadido en el grafo g
+    //Post: El nodo n, que representa el planeta p, se ha aÃ±adido en el grafo g
 	public  void afegir_planeta(Planeta p)
 	{
 		Nodo n = transformar_PlanetaaNodo(p);
@@ -34,14 +36,14 @@ public class Entrada {
 	}
 	
     //Pre: La ruta r existe en la galaxia
-    //Post: El arco a, que representa la ruta r, se ha añadido en el grafo g
+    //Post: El arco a, que representa la ruta r, se ha aÃ±adido en el grafo g
 	public void afegir_ruta(Ruta r, ControladorPlaneta cp)
 	{
 		Planeta origen = cp.BuscarPlaneta( r.consultar_planetaA() );
 		Planeta desti = cp.BuscarPlaneta( r.consultar_planetaB() );
 		Nodo o = transformar_PlanetaaNodo(origen);
 		Nodo d = transformar_PlanetaaNodo(desti);
-		Arco a = new Arco(r.consultar_id(), o, d, r.consultar_capacidad());
+		Arco a = new Arco(, o, d, r.consultar_capacidad(), r.consultar_id());
 		g.addArco(a);		
-	}
+	}*/
 }
