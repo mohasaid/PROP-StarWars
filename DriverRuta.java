fa46d996f3a2fda5d329ea3d1fbcf48ceede1ab6
Gerard9494 <gerard9494@gmail.com>
2014-11-12 18:27:51 +0100

    Update DriverRuta.java

--- DriverRuta.java
+++ DriverRuta.java
@@ -11,26 +11,29 @@ public class DriverRuta {
                 "-----------------------------------------------------------------------------------"
                 +   "                               DRIVER RUTA                                      "
                 +   "-----------------------------------------------------------------------------------"
-                + "\n\nCREACIÓ Ruta\n"
+                + "\n\nCREACIO RUTA\n"
                 + " 0: Sortir de la gestio del driver \n"
-                + " 1: Crear una Ruta buida -> public Ruta() \n"
-                + " 2: Crear una Ruta amb un identificador, una capacitat, una distancia, un planetaA, un planetaB i un valor boolea -> public CrearRuta(int id, int capacidad, int distancia, int planetaA, int planetaB, boolean bidireccional) \n"
+                + " 1: Ruta() \n"
+                + " 2: CrearRuta(int id, int capacidad, int distancia, int planetaA, int planetaB, boolean bidireccional) \n"
                 
                 + "\n\nOPERACIONS DE CONSULTA DE RUTA\n"
-                + " 3: Consultar l'identificador de la ruta -> public int consultar_id() \n"    
-                + " 4: Consultar la capacitat de la ruta -> public int consultar_capacidad() \n"       
-                + " 5: Consultar la distancia de la ruta -> public int consultar_distancia() \n"       
-                + " 6: Consultar l'identificador del planetaA de la ruta -> public int consultar_planetaA() \n"       
-                + " 7: Consultar l'identificador del planetaB de la ruta -> public int consultar_planetaB() \n"       
-                + " 8: Consultar si la ruta es bidireccional -> public boolean consultar_bidireccional() \n"
+                + " 3: consultar_id() \n"    
+                + " 4: consultar_capacidad() \n"       
+                + " 5: consultar_distancia() \n"       
+                + " 6: consultar_planetaA() \n"       
+                + " 7: consultar_planetaB() \n"       
+                + " 8: consultar_bidireccional() \n"
                 
                 + "\n\nOPERACIONS MODIFICADORES DE RUTA\n"
-                + " 9: Modificar l'identificador de la ruta -> public int modificar_id() \n"    
-                + " 10: Modificar la capacitat de la ruta -> public int modificar_capacidad() \n"       
-                + " 11: Modificar la distancia de la ruta -> public int modificar_distancia() \n"       
-                + " 12: Modificar l'identificador del planetaA de la ruta -> public int modificar_planetaA() \n"       
-                + " 13: Modificar l'identificador del planetaB de la ruta -> public int modificar_planetaB() \n"       
-                + " 14: Modificar si la ruta es bidireccional -> public boolean modificar_bidireccional() \n");
+                + " 9:  modificar_id() \n"    
+                + " 10: modificar_capacidad() \n"       
+                + " 11: modificar_distancia() \n"       
+                + " 12: modificar_planetaA() \n"       
+                + " 13: modificar_planetaB() \n"       
+                + " 14: modificar_bidireccional() \n"
+                + " 15: invertir_planetas() \n"
+
+    			);
     }
     
     public void Executa(Ruta r) throws Exception {
@@ -54,93 +57,173 @@ public class DriverRuta {
             case 12: TestModificarPlanetaARuta(cin);break;
             case 13: TestModificarPlanetaBRuta(cin);break;
             case 14: TestModificarBidireccionalRuta(cin);break;
+            case 15: TestInvertirPlanetasRuta();
             default: System.out.println("Opcio incorrecte");
             }
         opciones();
         n = cin.nextInt();
         }
     }
-     
+    
+    public void TestInvertirPlanetasRuta() throws Exception {
+    	try{
+    		r.invertir_planetas();
+    	}
+		catch (Exception e){
+	        System.out.print(e);
+		}
+    }
+    
 	public void TestCrearRutaBuida()
 	{
-		r = new Ruta();
+		try {
+			r = new Ruta();
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void  TestCrearRuta2(Scanner cin) throws Exception
 	{
-		int id = cin.nextInt();
-		int capacidad = cin.nextInt();
-		int distancia = cin.nextInt();
-		int planetaA = cin.nextInt();
-		int planetaB = cin.nextInt();
-		boolean bidireccional;
-		if ( cin.nextInt()==1 ) bidireccional = true;
-		else bidireccional = false;
-		r =  new Ruta(id, capacidad, distancia, planetaA, planetaB, bidireccional);
+		try {
+			int id = cin.nextInt();
+			int capacidad = cin.nextInt();
+			int distancia = cin.nextInt();
+			int planetaA = cin.nextInt();
+			int planetaB = cin.nextInt();
+			boolean bidireccional;
+			if ( cin.nextInt()==1 ) bidireccional = true;
+			else bidireccional = false;
+			r =  new Ruta(id, capacidad, distancia, planetaA, planetaB, bidireccional);
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void TestConsultarIdRuta() throws Exception
 	{
-		System.out.println(r.consultar_id());
+		try {
+			System.out.println(r.consultar_id());
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void TestConsultarCpacitatRuta() throws Exception
 	{
-		System.out.println(r.consultar_capacidad());
+		try {
+			System.out.println(r.consultar_capacidad());
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void TestConsultarDistanciaRuta() throws Exception
 	{
-		System.out.println(r.consultar_distancia());
+		try {
+			System.out.println(r.consultar_distancia());
+		} 
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void TestConsultarPlanetaARuta() throws Exception
 	{
-		System.out.println(r.consultar_planetaA());
+		try {
+			System.out.println(r.consultar_planetaA());
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void TestConsultarPlanetaBRuta() throws Exception
 	{
-		System.out.println(r.consultar_planetaB());
+		try {
+			System.out.println(r.consultar_planetaB());
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 		
 	public void TestConsultarBidireccionalRuta() throws Exception
 	{
-		if (r.consultar_bidireccional()) System.out.println("La ruta es bidireccional");
-		else System.out.println("La ruta no es bidireccional");
+		try {
+			if (r.consultar_bidireccional()) System.out.println("La ruta es bidireccional");
+			else System.out.println("La ruta no es bidireccional");
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
+		
 	}
 	
 	public void TestModificarIdRuta(Scanner cin) throws Exception 
 	{
-		
-		r.modificar_id(cin.nextInt());
+		try {
+			r.modificar_id(cin.nextInt());
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void TestModificarCpacitatRuta(Scanner cin) throws Exception 
 	{
-		r.modificar_capacidad(cin.nextInt());
+		try {
+			r.modificar_capacidad(cin.nextInt());
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void TestModificarDistanciaRuta(Scanner cin) throws Exception 
 	{
-		r.modificar_distancia(cin.nextInt());
+		try{ 
+			r.modificar_distancia(cin.nextInt());
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void TestModificarPlanetaARuta(Scanner cin) throws Exception 
 	{
-		r.modificar_planetaA(cin.nextInt());
+		try {
+			r.modificar_planetaA(cin.nextInt());
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 	
 	public void TestModificarPlanetaBRuta(Scanner cin) throws Exception 
 	{
-		r.modificar_planetaB(cin.nextInt());
+		try {
+			r.modificar_planetaB(cin.nextInt());
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 		
 	public void TestModificarBidireccionalRuta(Scanner cin) throws Exception 
 	{
-		boolean bid;
-		if (cin.nextInt() == 0) bid = false;
-		else bid = true;
-		r.modificar_bidireccional(bid);
+		try {
+			boolean bid;
+			if (cin.nextInt() == 0) bid = false;
+			else bid = true;
+			r.modificar_bidireccional(bid);
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
 	}
 }
