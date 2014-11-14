7ad8636c55e036b872d7b434510bd152baed603e
elenaouro <elenaouro@gmail.com>
2014-11-14 20:47:15 +0100

    Update TipoNave4.java

--- TipoNave4.java
+++ TipoNave4.java
@@ -21,28 +21,34 @@ public class TipoNave4 extends Nave{
 	public TipoNave4(){
 		ident = -1;
 		destino =-1;
+		consumo = -1;
 	}
 	//pre:cierto;
 	//Post: Crea una nave de tipo 4 a partir de una nave vacía
 	public TipoNave4(Nave n) throws Exception{
 		ident = n.consultar_id();
 		destino = n.consultar_destino();
+		origen = n.consultar_origen();
 	}
 
 	//pre: el nombre de tipo t no existe ya
 	//post: crea una nave de tipo 1 inicializada
-	public TipoNave4(int id,int d) throws Exception{
+	public TipoNave4(int id,int d,int o) throws Exception{
 		if(ErrorTipograficoID(id)){
 			throw new Exception("Error: El identificador del tipo debe ser mayor o igual que 0\n");
 		}
 		if(ErrorTipograficoID(d)){
 			throw new Exception("Error: El identificador del planeta destino debe ser mayor o igual que 0\n");
 		}
+		if(ErrorTipograficoID(o)){
+			throw new Exception("Error: El identificador del planeta origen debe ser mayor o igual que 0\n");
+		}
 		if(!definido){
 			throw new Exception("Error: el tipo de nave 3 no ha sido definido\n");
 		}
 		ident = id;
 		destino = d;
+		origen = o;
 	}
 	
 	//Pre:cierto
