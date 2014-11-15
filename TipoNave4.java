bf85def825dd94bb0630536607d47ceef6375c43
elenaouro <elenaouro@gmail.com>
2014-11-15 15:15:43 +0100

    Update TipoNave4.java

--- TipoNave4.java
+++ TipoNave4.java
@@ -8,7 +8,13 @@ public class TipoNave4 extends Nave{
 	public static boolean ErrorTipografico(int i) {  
         	return (i<=0);
 	} 
-	
+	//pre:cierto
+	//post: el tipo de nave 4 queda definido pasando a tener consumo c asignado
+	/**
+	 * metodo que define el tipo de nave 4 asignandole un consumo
+	 * @param c
+	 * @throws Exception
+	 */
 	public static void DefinirTipo(int c) throws Exception{
 		if(ErrorTipografico(c)){
 			throw new Exception("Error: el consumo debe ser mayor que 0\n");
@@ -18,21 +24,36 @@ public class TipoNave4 extends Nave{
 	}
 	//pre: cierto
 	//post: crea una nave de tipo 4 vacía
+	/**
+	 * metodo que crea una nave de tipo 1 vacía
+	 */
 	public TipoNave4(){
 		ident = -1;
 		destino =-1;
 		consumo = -1;
 	}
 	//pre:cierto;
-	//Post: Crea una nave de tipo 4 a partir de una nave vacía
+	//Post: Crea una nave de tipo 4 a partir de una nave sin tipo
+	/**
+	 * metodo que crea una nave de tipo 4 a partir de una nave sin tipo
+	 * @param n
+	 * @throws Exception
+	 */
 	public TipoNave4(Nave n) throws Exception{
 		ident = n.consultar_id();
 		destino = n.consultar_destino();
 		origen = n.consultar_origen();
 	}
 
-	//pre: el nombre de tipo t no existe ya
+	//pre: cierto
 	//post: crea una nave de tipo 1 inicializada
+	/**
+	 * metodo que crea una nave de tipo 4 con identificador, destino y origen
+	 * @param id
+	 * @param d
+	 * @param o
+	 * @throws Exception
+	 */
 	public TipoNave4(int id,int d,int o) throws Exception{
 		if(ErrorTipograficoID(id)){
 			throw new Exception("Error: El identificador del tipo debe ser mayor o igual que 0\n");
@@ -53,18 +74,29 @@ public class TipoNave4 extends Nave{
 	
 	//Pre:cierto
 		//Post devuelve el estado del tipo de nave.
+	/**
+	 * metodo que crea consulta si el tipo 4 está definido
+	 */
 		public boolean EstaDefinido(){
 			return definido;
 		}
 		
 	//pre: cierto
 	//post: retorna el nombre que identifica al tipo
+		/**
+		 * metodo que consulta el tipo de una nave
+		 * @throws Exception
+		 */
 	public int consultar_tipo() throws Exception{
 		return 4;
 	}
 
 	//pre: cierto
 	//post: retorna el consumo asociado al tipo
+	/**
+	 * metodo que consulta el consumo asociado al tipo de nave 4
+	 * @throws Exception
+	 */
 	public int consultar_consumo() throws Exception{
 		if(consumo == 0){
 		throw new Exception("Error: El tipo de nave no tiene un consumo asociado\n");	
@@ -74,6 +106,11 @@ public class TipoNave4 extends Nave{
 
 	//pre: cierto
 	//post: modifica el consumo asociado al tipo de nave
+	/**
+	 * metodo que modifica el consumo asociado al tipo de nave 4
+	 * @param c
+	 * @throws Exception
+	 */
 	public static void modificar_consumo(int c) throws Exception{
 		if(ErrorTipografico(c)){
 			throw new Exception("Error: El consumo debe de ser superior a 0\n");	
@@ -94,4 +131,3 @@ class OrdenTipoNave4 implements Comparator<TipoNave4>{
         return 0;
     }
 }
-
