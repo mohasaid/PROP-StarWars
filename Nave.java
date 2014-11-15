3e83fba6b0ade94a6fb063c7eb95abb1425fe8d7
elenaouro <elenaouro@gmail.com>
2014-11-15 15:16:28 +0100

    Update Nave.java

--- Nave.java
+++ Nave.java
@@ -12,6 +12,9 @@ public class Nave {
 
 	//pre: cierto
 	//post: El resultado es una nave vacía
+	/**
+	 * metodo que crea una nave vacía
+	 */
 	public Nave(){
 		ident = -1;
 		destino = -1;
@@ -21,6 +24,13 @@ public class Nave {
 	
 	//pre: El planeta destino d existe en la galaxia y el identificador id no está un uso
 	//post: El resultado es una Nave sin tipo
+	/**
+	 * metodo que crea una nave con identificador, destino y origen.
+	 * @param id
+	 * @param d
+	 * @param o
+	 * @throws Exception
+	 */
 	public Nave(int id, int d, int o) throws Exception {
 		if(ErrorTipograficoID(id)){
 		        throw new Exception("Error : El identificador de una nave debe ser mayor o igual que 0\n"); 
@@ -38,6 +48,10 @@ public class Nave {
 
 	//pre: cierto
 	//post: retorna un entero que representa el identificador de la nave
+	/**
+	 * metodo que consulta el identificador de una nave
+	 * @throws Exception
+	 */
 	public int consultar_id() throws Exception{
 		if(ident == -1){
 			throw new Exception("Error: La nave no tiene un id asignado\n");
@@ -47,6 +61,9 @@ public class Nave {
 
 	//pre: cierto
 	//post: retorna un entero que representa el identificador del planeta al que la nave se dirige
+	 /** metodo que consulta el destino de una nave
+	 * @throws Exception
+	 */
 	public int consultar_destino(){
 
 		return destino;
@@ -54,11 +71,18 @@ public class Nave {
 	}
 	//pre:cieto
 	//post: retorna un entero que representa el identificador del planeta en el que se encuentra la nave inicialmente
+	/** metodo que consulta el origen de una nave
+	 * @throws Exception
+	 */
 	public int consultar_origen(){
 		return origen;
 	}
 	//pre: cierto
 	//post: la id se ha modificado por el entero id
+	/** metodo que modifica el identificador de una nave
+	 * @param id
+	 * @throws Exception
+	 */
 	public void modificar_id(int id) throws Exception{
 		if(ErrorTipograficoID(id)) {  
 		        throw new Exception("Error : El identificador de una nave debe ser mayor o igual que 0\n"); 
@@ -67,6 +91,10 @@ public class Nave {
 	}
 	//pre: El planeta destino idD existe en la galaxia
 	//post: se ha modificado el destino de la nave por idD
+	/** metodo que modifica el destino de una nave
+	 * @param idD
+	 * @throws Exception
+	 */
 	public void modificar_destino(int idD) throws Exception {
 		if(ErrorTipograficoID(idD)){
 			throw new Exception("Error: El identificador del planeta destino ha de ser mayor o igual que 0\n");	
@@ -75,6 +103,10 @@ public class Nave {
 	}
 	//pre: El planeta origen idO existe en la galaxia
 	//post: se ha modificado el origen por el planeta con identificador idO
+	/** metodo que modifica el origen de una nave
+	 * @param idO
+	 * @throws Exception
+	 */
 	public void modificar_origen(int idO) throws Exception {
 		if(ErrorTipograficoID(idO)){
 			throw new Exception("Error: El identificador del planeta origen ha de ser mayor o igual que 0\n");	
