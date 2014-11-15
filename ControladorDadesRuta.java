6f2edac42fa16650b98e9acb2fcab3dfb5cdaac3
Gerard9494 <gerard9494@gmail.com>
2014-11-15 16:52:06 +0100

    Create ControladorDadesRuta.java

---
+++ ControladorDadesRuta.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+
+public class ControladorDadesRuta extends ControladorDades {
+	
+    private boolean inicial;
+    
+    //CONSTRUCTORA 
+	public ControladorDadesRuta() {
+		// TODO Auto-generated constructor stub
+	}
+	
+    //MODIFICADORA
+    public void Inicial(boolean b) {
+        inicial = b;
+    }
+    
+    //METODES
+   public String cargar(String path) throws FileNotFoundException 
+    {     
+        String res = "";
+        FileReader fr = new FileReader(path);
+        Scanner in = new Scanner(fr);
+        while (in.hasNextLine()) {
+            res += in.nextLine();
+            res += "@";
+        }
+        in.close();
+        return res;
+    }
+     
+    public void guardar(String path, String res)throws IOException  
+    {        
+        PrintWriter out;
+         
+        if (inicial) {
+            out = new PrintWriter(path);
+        }
+        else {
+            out = new PrintWriter(new BufferedWriter(new FileWriter(path, true)));
+        }
+        Scanner scan = new Scanner(res);
+        scan.useDelimiter("@");
+        String s;
+        while(scan.hasNext()) {
+            s = scan.next();
+            out.println(s);            
+        }
+        scan.close();
+        out.close();        
+    }
+
+}
