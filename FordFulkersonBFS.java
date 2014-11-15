20218ac947e9f4068c5b02b349e9298f7b791c60
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-16 00:00:50 +0100

    Update FordFulkersonBFS.java

--- FordFulkersonBFS.java
+++ FordFulkersonBFS.java
@@ -16,3 +16,30 @@ public class FordFulkersonBFS extends FordFulkerson{
 	}
 	
 }
+
+
+/*
+public static boolean bfs_Grafo(ArrayList<ArrayList<Pair<Arco, Integer> > > g, int or, int dest, int path[])
+{
+	int V = g.size();
+	boolean[] visitados = new boolean[V];
+	Arrays.fill(visitados,false);
+	visitados[or] = true; // Primero lo visitamos
+	Queue<Integer> q1 = new LinkedList<Integer>(); // id nodos a visitar
+	q1.add(or);
+	while(!q1.isEmpty()) {
+		int actual = q1.poll().intValue(); // nodo actual
+		int size = g.get(actual).size();
+		for(int i = 0; i < size; ++i) {
+			int v = g.get(actual).get(i).consultarSegundo(); // nodo destino
+			int c = g.get(actual).get(i).consultarPrimero().ConsultarCapacidad();
+			if(c != 0 && !visitados[v]) {
+				path[v] = actual;
+				visitados[v] = true;
+				q1.add(v);
+			}
+		}
+	}
+	return visitados[dest];
+}
+*/
