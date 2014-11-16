bf68400c0142fcee497231cd8aac799106f18842
elenaouro <elenaouro@gmail.com>
2014-11-16 04:41:30 +0100

    Update FuncionFlujo.java

--- FuncionFlujo.java
+++ FuncionFlujo.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
 public class FuncionFlujo extends FuncionesCoste{
-	public void FuncionFlujo(){}
-	public void CalcularCoste(Arco a, int distancia, int precio){
-		a.ModificarCoste(0);
+	public FuncionFlujo(){}
+	public int CalcularCoste(){
+		return 0;
 	}
 }
+
