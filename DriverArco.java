fd020c5362dbfcaa41d6d273954e05127d2b2653
elenaouro <elenaouro@gmail.com>
2014-11-16 22:07:16 +0100

    Update DriverArco.java

--- DriverArco.java
+++ DriverArco.java
@@ -6,30 +6,22 @@ public class DriverArco{
     public void Executa(Scanner sc) {
         int op;
         Arco a = new Arco();
-        Nodo aux1 = new Nodo(1,false,false);
-        Nodo aux2 = new Nodo(2,false,false);
 
         
                 		System.out.print("Opciones: \n\n"
-		         +"--------------------------------------------------------------------\n"
+		     +"--------------------------------------------------------------------\n"
       	     +"-                    OPCIONES                                      -\n"
    	         +"-           [opcion   Operacion(Atributos)                         -\n"
              +"--------------------------------------------------------------------\n"
              +"-                                                                  -\n"
              +"-   0   Salir del DriverArco                                       -\n"
              +"-   1   TestArco(int id, int capacidad,int idRuta)                 -\n"
-             +"-   2   TestConsultarId()                                          -\n"
-             +"-   3   TestConsultarOrigen()                                      -\n"
-             +"-   4   TestConsultarDestino()                                     -\n"
-             +"-   5   TestConsultarCapacidad()                                   -\n"
-             +"-   6   TestConsultarCoste()                                       -\n"
-             +"-   7   TestConsultarIdRuta()                                      -\n"
-             +"-   8   TestModificarId(int NuevoId)                               -\n"
-             +"-   9   TestModificarOrigen()                                      -\n"
-             +"-   10  TestModificarDestino()                                     -\n"
-             +"-   11  TestModificarCapacidad(int NuevaCapacidad)                 -\n"
-             +"-   12  TestModificarCoste(int NuevoCoste)                         -\n"
-             +"-   13  TestModificarIdRuta(int NuevoIDRuta)                       -\n"
+             +"-   2   TestConsultarCapacidad()                                   -\n"
+             +"-   3   TestConsultarCoste()                                       -\n"
+             +"-   4   TestConsultarIdRuta()                                      -\n"
+             +"-   5  TestModificarCapacidad(int NuevaCapacidad)                 -\n"
+             +"-   6  TestModificarCoste(int NuevoCoste)                         -\n"
+             +"-   7  TestModificarIdRuta(int NuevoIDRuta)                       -\n"
              +"-                                                                  -\n"
              +"--------------------------------------------------------------------\n");
 
@@ -37,31 +29,20 @@ public class DriverArco{
                 
         while(op != 0){
             switch (op) {
-            case 1: TestArco(sc,a,aux1,aux2);break;
-            case 2: TestConsultarId(a);break;
-            case 3: TestConsultarOrigen(a);break;
-            case 4: TestConsultarDestino(a);break;
-            case 5: TestConsultarCapacidad(a);break;
-            case 6: TestConsultarCoste(a);break;
-            case 7: TestConsultarIdRuta(a);break;
-            case 8: TestModificarId(sc,a);break;
-            case 9: TestModificarOrigen(a,aux1,aux2);break;
-            case 10: TestModificarDestino(a,aux1,aux2);break;
-            case 11: TestModificarCapacidad(sc,a);break;
-            case 12: TestModificarCoste(sc,a);break;
-            case 13: TestModificarIdRuta(sc,a);break;
+            case 1: TestArco(sc,a);break;
+            case 2: TestConsultarCapacidad(a);break;
+            case 3: TestConsultarCoste(a);break;
+            case 4: TestConsultarIdRuta(a);break;
+            case 5: TestModificarCapacidad(sc,a);break;
+            case 6: TestModificarCoste(sc,a);break;
+            case 7: TestModificarIdRuta(sc,a);break;
             default: System.out.println("Opción incorrecta");
             }
     op = sc.nextInt(); 
     }
    }
-	public static void TestArco(Scanner sc, Arco a, Nodo aux1, Nodo aux2){
+	public static void TestArco(Scanner sc, Arco a){
 		try{
-	    	while(!sc.hasNextInt()){
-	            String s = sc.nextLine();
-	            throw new Exception("Error: El identificador debe ser un entero\n");
-	        }
-	    	int id = sc.nextInt();
 	    	while(!sc.hasNextInt()){
 	            String s = sc.nextLine();
 	            throw new Exception("Error: La capacidad debe ser un entero mayor que 0\n");
@@ -72,38 +53,14 @@ public class DriverArco{
 	            throw new Exception("Error: El identificador de la ruta original debe ser un entero\n");
 	        }
 	    	int idr = sc.nextInt();
-	    	a = new Arco(id,aux1,aux2,c,idr);
+	    	a = new Arco(c,idr);
     	}
     	 catch (Exception e){
              System.out.print(e);
      }
 	}
 	//CONSULTORAS
-	public static void TestConsultarId(Arco a){
-		try{
-	    	System.out.println(a.ConsultarId());
-    	}
-    	 catch (Exception e){
-             System.out.print(e);
-    	 }
-	}
-	
-	public static void TestConsultarOrigen(Arco a){
-		try{
-	    	System.out.println(a.ConsultarOrigen().consultar_id());
-    	}
-    	 catch (Exception e){
-             System.out.print(e);
-	}
-	}
-	public static void TestConsultarDestino(Arco a){
-		try{
-	    	System.out.println(a.ConsultarDestino().consultar_id());
-    	}
-    	 catch (Exception e){
-             System.out.print(e);
-	}
-	}
+
 	public static void TestConsultarCapacidad(Arco a){
 		try{
 	    	System.out.println(a.ConsultarCapacidad());
@@ -130,38 +87,6 @@ public class DriverArco{
 	     }
 	}
 	//MODIFICADORAS
-	public static void TestModificarId(Scanner sc, Arco a){
-		try{
-			while(!sc.hasNextInt()){
-	            String s = sc.nextLine();
-	            throw new Exception("Error: El identificador debe ser un entero\n");
-	        }
-	    	int idn = sc.nextInt();
-	    	a.ModificarId(idn);
-    	}
-    	 catch (Exception e){
-             System.out.print(e);
-     }
-	}
-
-	public static void TestModificarOrigen(Arco a, Nodo aux1, Nodo aux2){
-		try{
-			if(a.ConsultarOrigen().consultar_id()==1) a.ModificarOrigen(aux2);
-			else a.ModificarOrigen(aux1);
-    	}
-    	 catch (Exception e){
-             System.out.print(e);
-    	 }
-	}
-	public static void TestModificarDestino(Arco a,Nodo aux1, Nodo aux2){
-		try{
-			if(a.ConsultarDestino().consultar_id()==1) a.ModificarDestino(aux2);
-			else a.ModificarDestino(aux1);
-    	}
-    	 catch (Exception e){
-             System.out.print(e);
-    	 }
-	}
 	public static void TestModificarCapacidad(Scanner sc, Arco a){
 		try{
 			while(!sc.hasNextInt()){
@@ -175,6 +100,7 @@ public class DriverArco{
              System.out.print(e);
      }
 	}
+
 	public static void TestModificarCoste(Scanner sc, Arco a){
 		try{
 			while(!sc.hasNextInt()){
