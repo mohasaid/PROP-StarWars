fa7794e914bc96c7acdb113672381a882d28561d
elenaouro <elenaouro@gmail.com>
2014-11-16 04:40:14 +0100

    Update FuncionesCoste.java

--- FuncionesCoste.java
+++ FuncionesCoste.java
@@ -1,8 +1,17 @@
 import java.util.*;
 
 public abstract class FuncionesCoste{
+	protected Ruta r;
+	protected Planeta p;
 	public FuncionesCoste(){
+		r = new Ruta();
+		p = new Planeta();
 	}
-	public abstract Entrada Calcular(Pair<List<Arco>,List<Nodo>> pl ,Entrada e);
+	public void ModificarRuta(Ruta r1){
+		r = r1;
+	}
+	public void ModificarPlaneta(Planeta p1){
+		p = p1;
+	}
+	public abstract int CalcularCoste() throws Exception;
 }
-
