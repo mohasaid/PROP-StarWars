c0c20fcbac6a233188f3476c2e6408b48a88880b
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-16 02:32:49 +0100

    Update and rename FordFulkerson(1).java to FordFulkerson.java

--- FordFulkerson.java
+++ FordFulkerson.java
@@ -9,10 +9,44 @@ public /*abstract*/ class FordFulkerson extends MFP{
 		g_residual = new Grafo();
 		s = new Salida();
 	}
-	public void Ejecutar(){
-		//Codigo Parte comun
+	public int Ejecutar(int origen, int destino){
+		int size = g.sizeGrafo();
+		
+		// g_residual = g;
+		
+		int path[] = new int[size];
+		Arrays.fill(path, -1);
+		int dist[] = new int[size];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		
+		int u,v;
+		int max_flow = 0;
+		
+		while(BFS O DIJKSTRA) {
+		
+		int pathflow = Integer.MAX_VALUE;
+		for(v = destino; v != origen; v = path[v]) {
+			u = path[v];
+			pathflow = Math.min(pathflow, g_residual.consultaPairUn(u, v).consultarPrimero().ConsultarCapacidad());
+		}
+		for(v = destino; v != origen; v = path[v]) {
+			u = path[v];
+			int tmp = g_residual.consultaPairUn(u, v).consultarPrimero().ConsultarCapacidad();
+			tmp -= pathflow;
+			g_residual.ponEnVerAr(u, v, tmp);
+			
+			int tmp1 = g_residual.consultaPairUn(v, u).consultarPrimero().ConsultarCapacidad();
+			tmp1 += pathflow;
+			
+			g_residual.ponEnVerAr(v, u, tmp1); // poner solo una capacidad
+		}
+		max_flow += pathflow;
 	}
+	return max_flow;
+	}
+
 }
+
 
 /*public static int MaxFflowDjik(ArrayList<ArrayList<Pair<Integer, Integer> > > g, int origen, int destino)
 {
