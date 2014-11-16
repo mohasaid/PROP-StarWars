c656548000878147d989d51f96303af238b75482
elenaouro <elenaouro@gmail.com>
2014-11-16 04:43:01 +0100

    Update FuncionDistancia.java

--- FuncionDistancia.java
+++ FuncionDistancia.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class FuncionDistancia extends FuncionesCoste{
-	public FuncionDistancia(){}
+	public FuncionDistancia(){
+		r = new Ruta();
+	}
 	public int CalcularCoste() throws Exception{
 		return r.consultar_distancia();
 	}
