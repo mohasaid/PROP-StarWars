633013d275689692e1d16f3f45a5b84aa80da8e2
elenaouro <elenaouro@gmail.com>
2014-11-16 22:40:28 +0100

    Update MFP.java

--- MFP.java
+++ MFP.java
@@ -2,17 +2,11 @@ import java.util.*;
 
 public abstract class MFP{
 	protected Salida s;
-	public MFP(){
-		s = new Salida();
-	}
 	public ArrayList<Integer> ConsultarCuellos(){
 		return s.ConsultarCuellos();
 		}
 		public ArrayList<Pair<Arco,Integer>> ConsultarFlujos(){
 		return s.ConsultarFlujos();
-		}
-		public int ConsultarCoste(){
-		return s.ConsultarCost();
 		}
 		public int size(){
 			return s.size();
