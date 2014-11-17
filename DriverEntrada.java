23f1043e1e12b8e575b4add0a77205ef7664915b
Gerard9494 <gerard9494@gmail.com>
2014-11-17 18:34:08 +0100

    Update DriverEntrada.java

--- DriverEntrada.java
+++ DriverEntrada.java
@@ -7,27 +7,27 @@ public class DriverEntrada {
     public void opciones()
     {
     	System.out.print(
-    				"-----------------------------------------------------------------------------------"
-                +   "                               DRIVER ENTRADA                                      "
-                +   "-----------------------------------------------------------------------------------"
+    				"-----------------------------------------------------------------------------------\n"
+                +   "                               DRIVER ENTRADA                                      \n"
+                +   "-----------------------------------------------------------------------------------\n"
                 +   "\n\nCREACIO ENTRADA\n"
                 +   " 0: Salir de la gestion del driver \n"
                 +   " 1: Entrada() \n"
-                +   " 2: Entrada(ControladorGalaxia cg) \n"
+                +   " 2: Entrada(ControladorGalaxia cg, ControladorRuta cr, ControladorPlaneta cp, ControladorNave cn) \n"
                 
                 +   "\n\nOPERACIONES DE CONSULTA DE ENTRADA\n"
                 +   " 3: Consultar_grafo() \n"    
     			);
     }
     
-    public void Executa(Entrada en, ControladorGalaxia cg) throws Exception {
+    public void Executa(Entrada en, ControladorGalaxia cg, ControladorRuta cr, ControladorPlaneta cp, ControladorNave cn) throws Exception {
         e = en;
         opciones();
         int op = cin.nextInt();
         while(op != 0) {
             switch(op) {
             case 1:  TestCrearEntradaVacia();break;
-            case 2:  TestCrearEntrada2(cg);break;
+            case 2:  TestCrearEntrada2(cg, cr, cp, cn);break;
             case 3:  TestConsultarGrafo();break;
             default: System.out.println("Opcio incorrecte");
             }
@@ -46,10 +46,10 @@ public class DriverEntrada {
 		}
     }
     
-    public void TestCrearEntrada2(ControladorGalaxia cg)
+    public void TestCrearEntrada2(ControladorGalaxia cg, ControladorRuta cr, ControladorPlaneta cp, ControladorNave cn)
     {
 		try {
-			e = new Entrada(cg);
+			e = new Entrada(cg, cr, cp, cn);
 		}
 		catch (Exception e){
             System.out.print(e);
