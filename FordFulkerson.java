f3cc8d59be3f1987c4c1d148b65dfb0430dd1fcb
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-17 18:42:23 +0100

    Update FordFulkerson.java

--- FordFulkerson.java
+++ FordFulkerson.java
@@ -1,41 +1,57 @@
 import java.util.*;
 
-
 public abstract class FordFulkerson extends MFP{
 	protected int path[];
 	protected int dist[];
 
 	public void Ejecutar(){
 		int size = g.sizeGrafo();
-		// g_residual = g;
+		g_residual = g;
+		
+		int origen = size-1;
+		int destino = size-2;
+		
 		int path[] = new int[size];
 		Arrays.fill(path, -1);
+		
 		int dist[] = new int[size];
 		Arrays.fill(dist, Integer.MAX_VALUE);
+		
 		int u,v;
 		int max_flow = 0;
-		while(BFS O DIJKSTRA) {
+		String res = "Buscamos camino de " + origen + " hacia " + destino + " para poder aumentar flujo";
+		s.AnadirCambio(res); // en salida[0] = buscar camino
+		while(Recorrido(origen,destino)) {
 			int pathflow = Integer.MAX_VALUE;
+			res = "Empezamos en nodo " + destino + " y mientras sea diferente de " + origen + ".";
 			for(v = destino; v != origen; v = path[v]) {
 				u = path[v];
+				res += "Buscamos menor capacidad residual valor entre los nodos del camino. ";
 				pathflow = Math.min(pathflow, g_residual.consultaPairUn(u, v).consultarPrimero().ConsultarCapacidad());
+				res += "La menor capacidad residual ha resultado ser " + pathflow + ".";
 			}
+			res += "Actualizamos la capacidad residual entre los arcos del camino. ";
 			for(v = destino; v != origen; v = path[v]) {
 				u = path[v];
+				res += "Actualizamos la capacidad residual entre los arcos " + u + " y " + v + ".";
 				int tmp = g_residual.consultaPairUn(u, v).consultarPrimero().ConsultarCapacidad();
 				tmp -= pathflow;
 				g_residual.ponEnVerAr(u, v, tmp);
 				int tmp1 = g_residual.consultaPairUn(v, u).consultarPrimero().ConsultarCapacidad();
 				tmp1 += pathflow;
-				g_residual.ponEnVerAr(v, u, tmp1); // poner solo una capacidad
+				g_residual.ponEnVerAr(v, u, tmp1);
 			}
+			res += " Actualizamos flujo de ruta al flujo general.";
 			max_flow += pathflow;
+			s.AnadirCambio(res); // salida[i] = iteracion i-1
 		}
-		return max_flow;
+		s.AnadirMax_flow(max_flow);
 	}
+	
 	public void Caminos(int idNave, int consumo){
 		super.Caminos(idNave, consumo);
 	}
 	public abstract boolean Recorrido(int origen, int destino);
 	
 }
+
