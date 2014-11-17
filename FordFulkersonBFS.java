ae9702b84b587e7df95abbd1f3e55811eea104ee
elenaouro <elenaouro@gmail.com>
2014-11-17 13:38:34 +0100

    Update FordFulkersonBFS.java

--- FordFulkersonBFS.java
+++ FordFulkersonBFS.java
@@ -18,6 +18,9 @@ import java.util.Queue;
 			g_residual = e.Consultar_grafo();
 			s = new Salida();
 		}
+		public void Caminos(int idNave, int consumo){
+			super.Caminos(idNave, consumo);
+		}
 		public int Ejecutar(int origen, int destino){
 			return super.Ejecutar(origen,destino);
 		}
