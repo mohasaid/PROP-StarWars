8e4ee3fc0d43651c0be40adc8f7286beb88dacbf
JoseMartinNogueira <josemn15@hotmail.com>
2014-11-17 23:05:16 +0100

    Update DriverPlaneta.java

--- DriverPlaneta.java
+++ DriverPlaneta.java
@@ -13,25 +13,25 @@ public class DriverPlaneta {
             + "Opciones \n"
             + " 1: Planeta(int id, int k, Pair<Integer,Integer> Coo)\n"
             + " 2: Consultar_id()\n"
-            + " 4: Consultar_Coste()\n"
-            + " 5: Consultar_Coordenadas()\n"
-            + " 6: consultar_X( )\n"
-            + " 7: consultar_Y( )\n"
-            + " 8: Modificar_Coste(int k)\n"
-            + " 9: modificarCoordenades(int x, int y)\n"
-            + " 10: Borrar()\n");
+            + " 3: Consultar_Coste()\n"
+            + " 4: Consultar_Coordenadas()\n"
+            + " 5: consultar_X( )\n"
+            + " 6: consultar_Y( )\n"
+            + " 7: Modificar_Coste(int k)\n"
+            + " 8: modificarCoordenades(int x, int y)\n"
+            + " 9: Borrar()\n");
         n = sc.nextInt();
         while(n != 0) {
             switch(n) {
                 case 1: TestCrearP(sc);break;
                 case 2: TestCId();break;
-                case 4: TestCCoste();break;
-                case 5: TestCCoordenadas();break;
-                case 6: TestCCoordenadaX();break;
-                case 7: TestCCoordenadaY();break;
-                case 8: TestMCoste(sc);break;
-                case 9: TestMCoordenadas(sc);break;
-                case 10: TestB();break;
+                case 3: TestCCoste();break;
+                case 4: TestCCoordenadas();break;
+                case 5: TestCCoordenadaX();break;
+                case 6: TestCCoordenadaY();break;
+                case 7: TestMCoste(sc);break;
+                case 8: TestMCoordenadas(sc);break;
+                case 9: TestB();break;
             }
             n = sc.nextInt();
         }
