3cc2c42f30e590b92a35e0d9d9c61e9afcd9db78
elenaouro <elenaouro@gmail.com>
2014-11-17 10:55:48 +0100

    Update FordFulkersonBFS.java

--- FordFulkersonBFS.java
+++ FordFulkersonBFS.java
@@ -3,92 +3,44 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class FordFulkersonBFS extends FordFulkerson {
-	private int path[]; // path como privado o la paso por la funcion
-	    int path[] = new int[size];
-		Arrays.fill(path, -1);
-	 
-	public FordFulkersonBFS(){
-		g = new Grafo();
-		g_residual = new Grafo();
-		s = new Salida();
-		
-	}
-	public FordFulkersonBFS(/*Entrada e){*/
-		g = e.Consultar_grafo();
-		g_residual = e.Consultar_grafo();
-		s = new Salida();
-	}
-	
-	public void Ejecutar(int origen, int destino){
-		int V = g_residual.sizeGrafo();
-		
-		boolean[] visitados = new boolean[V];
-		Arrays.fill(visitados,false);
-		
-		visitados[origen] = true;
-		Queue<Integer> q1 = new LinkedList<Integer>();
-		q1.add(origen);
-		
-		while(!q1.isEmpty()) {
-			int actual = q1.poll().intValue();
-			int size = g_residual.sizeGrafo(actual);
-			for(int i = 0; i < size; ++i) {
-				int adj = g_residual.consultarSeg(actual, i);
-				int cap = g_residual.consultarPrim(actual, i).ConsultarCapacidad();
-				if(cap != 0 && !visitados[adj]) {
-					path[adj] = actual;
-					visitados[adj] = true;
-					q1.add(adj);
+
+	public class FordFulkersonBFS extends FordFulkerson {
+		//private int path[]; // path como privado o la paso por la funcion
+	int path[] = new int[size];
+	Arrays.fill(path, -1);
+		public FordFulkersonBFS(){
+			g = new Grafo();
+			g_residual = new Grafo();
+			s = new Salida();
+		}
+		public FordFulkersonBFS(Entrada e){
+			g = e.Consultar_grafo();
+			g_residual = e.Consultar_grafo();
+			s = new Salida();
+		}
+		public int Ejecutar(int origen, int destino){
+			return super.Ejecutar(origen,destino);
+		}
+		public void Recorrido(int origen, int destino){
+			int V = g_residual.sizeGrafo();
+			boolean[] visitados = new boolean[V];
+			Arrays.fill(visitados,false);
+			visitados[origen] = true;
+			Queue<Integer> q1 = new LinkedList<Integer>();
+			q1.add(origen);
+			while(!q1.isEmpty()) {
+				int actual = q1.poll().intValue();
+				int size = g_residual.sizeGrafo(actual);
+				for(int i = 0; i < size; ++i) {
+					int adj = g_residual.consultarSeg(actual, i);
+					int cap = g_residual.consultarPrim(actual, i).ConsultarCapacidad();
+					if(cap != 0 && !visitados[adj]) {
+						path[adj] = actual;
+						visitados[adj] = true;
+						q1.add(adj);
+					}
 				}
 			}
 		}
 	}
-	public ArrayList<Integer> ConsultarCuellos(){	
-		return s.ConsultarCuellos();
-		}
-	public ArrayList<Pair<Arco,Integer>> ConsultarFlujos(){
-		return s.ConsultarFlujos();
-		}
-	public int size(){
-			return s.size();
-		}
-	public int sizeCambios(){
-			return s.size();
-		}
-	public void GenerarFlujos(Entrada e, ControladorRuta cr){
-			s.GenerarFlujos(e,cr);
-		}
-		//Cambiar:
-		public ArrayList<String> ConsultarCambios(){
-		return s.ConsultarCambios();
-		}
-		public abstract void Ejecutar();
-}
-
-}
-/*
-public static boolean bfs_Grafo(ArrayList<ArrayList<Pair<Arco, Integer> > > g, int or, int dest, int path[])
-{
-	int V = g.size();
-	boolean[] visitados = new boolean[V];
-	Arrays.fill(visitados,false);
-	visitados[or] = true; // Primero lo visitamos
-	Queue<Integer> q1 = new LinkedList<Integer>(); // id nodos a visitar
-	q1.add(or);
-	while(!q1.isEmpty()) {
-		int actual = q1.poll().intValue(); // nodo actual
-		int size = g.get(actual).size();
-		for(int i = 0; i < size; ++i) {
-			int v = g.get(actual).get(i).consultarSegundo(); // nodo destino
-			int c = g.get(actual).get(i).consultarPrimero().ConsultarCapacidad();
-			if(c != 0 && !visitados[v]) {
-				path[v] = actual;
-				visitados[v] = true;
-				q1.add(v);
-			}
-		}
 	}
-	return visitados[dest];
-}
-*/
