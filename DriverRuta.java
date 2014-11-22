8334b70b998a70fc2e40dde788bcbdbbedfce806
Gerard9494 <gerard9494@gmail.com>
2014-11-22 15:58:04 +0100

    Update DriverRuta.java

--- DriverRuta.java
+++ DriverRuta.java
@@ -11,61 +11,80 @@ public class DriverRuta {
                 +   "-----------------------------------------------------------------------------------\n"
                 + "\n\nCREACION RUTA\n"
                 + " 0: Salir de la gestion del driver \n"
-                + " 1: Ruta() \n"
-                + " 2: CrearRuta(int id, int capacidad, int distancia, int planetaA, int planetaB, boolean bidireccional) \n"
+                + " 1: CrearRuta(int id, int capacidad, int distancia, int planetaA, int planetaB, boolean bidireccional) \n"
                 
                 + "\n\nOPERACIONES DE CONSULTA DE RUTA\n"
-                + " 3: consultar_id() \n"    
-                + " 4: consultar_capacidad() \n"       
-                + " 5: consultar_distancia() \n"       
+                + " 2: consultar_id() \n"    
+                + " 3: consultar_capacidad() \n"       
+                + " 4: consultar_distancia() \n"       
+                + " 5: consultar_bidireccional() \n"       
+
                 
                 + "\n\nOPERACIONES MODIFICADORAS DE RUTA\n"
                 + " 6: modificar_id() \n"    
                 + " 7: modificar_capacidad() \n"       
-                + " 8: modificar_distancia() \n"       
+                + " 8: modificar_distancia() \n" 
+                + " 9: modificar_bidireccional() \n"       
+
 
     			);
     }
     
     public void Executa(Scanner cin) throws Exception {
         int n;
-        r = new Ruta();
         opciones();
         n = cin.nextInt();
         while(n != 0) {
             switch(n) {
-            case 1:  TestCrearRutaBuida();break;
-            case 2:  TestCrearRuta2(cin);break;
-            case 3:  TestConsultarIdRuta();break;
-            case 4:  TestConsultarCpacitatRuta();break;
-            case 5:  TestConsultarDistanciaRuta();break;
+            case 1:  TestCrearRuta2(cin);break;
+            case 2:  TestConsultarIdRuta();break;
+            case 3:  TestConsultarCpacitatRuta();break;
+            case 4:  TestConsultarDistanciaRuta();break;
+            case 5:  TestConsultarBidireccionalitatRuta(cin);break;
             case 6:  TestModificarIdRuta(cin);break;
-            case 7: TestModificarCpacitatRuta(cin);break;
-            case 8: TestModificarDistanciaRuta(cin);break;
+            case 7:  TestModificarCpacitatRuta(cin);break;
+            case 8:  TestModificarDistanciaRuta(cin);break;
+            case 9:  TestModificarBidireccionalitatRuta(cin);break;
             default: System.out.println("Opcion incorrecta");
             }
         opciones();
         n = cin.nextInt();
         }
     }
+	
     
-	public void TestCrearRutaBuida()
+	public void TestConsultarBidireccionalitatRuta(Scanner cin) throws Exception 
 	{
 		try {
-			r = new Ruta();
+			if (r.consultar_bidireccional()) System.out.print("La ruta es bidireccional \n");
+			else System.out.print("La ruta no es bidireccional \n");
+
+		}
+		catch (Exception e){
+            System.out.print(e);
+		}
+	}
+
+	
+	public void TestModificarBidireccionalitatRuta(Scanner cin) throws Exception
+	{
+		try {
+			boolean b = cin.nextBoolean();
+			r.modificar_bidireccional(b);
 		}
 		catch (Exception e){
             System.out.print(e);
 		}
 	}
 	
-	public void  TestCrearRuta2(Scanner cin) throws Exception
+	public void TestCrearRuta2(Scanner cin) throws Exception
 	{
 		try {
 			int id = cin.nextInt();
 			int capacidad = cin.nextInt();
 			int distancia = cin.nextInt();
-			r =  new Ruta(id, capacidad, distancia);
+			boolean b = cin.nextBoolean();
+			r =  new Ruta(id, capacidad, distancia, b);
 		}
 		catch (Exception e){
             System.out.print(e);
