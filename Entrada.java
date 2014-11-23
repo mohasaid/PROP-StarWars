f08b655244311a1cd0421b9db58f4372ee8aeff6
Gerard9494 <gerard9494@gmail.com>
2014-11-23 12:36:07 +0100

    Update Entrada.java

--- Entrada.java
+++ Entrada.java
@@ -1,14 +1,7 @@
 public class Entrada {
 	private Grafo g;
-	
-    //Pre: Cierto
-    //Post: Crea una entrada vacia
-    public Entrada()
-    {
-    	g = new Grafo();
-    }
-    
-    //Pre: Cierto
+
+	//Pre: Cierto
     //Post: Crea una entrada con el grafo inicializado a partir de de la lista de nodos y otra de arcos
     public Entrada(ControladorGalaxia cg, ControladorRuta cr, ControladorPlaneta cp, ControladorNave cn) throws Exception
     {
