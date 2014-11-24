10f110294bdd91ac52fcc2d3a709731f36d6b976
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-24 17:47:07 +0100

    Update Entrada.java

--- Entrada.java
+++ Entrada.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
+
 public class Entrada {
 	private Grafo g;
-
-	//Pre: Cierto
+	
+    //Pre: Cierto
     //Post: Crea una entrada con el grafo inicializado a partir de de la lista de nodos y otra de arcos
-    public Entrada(ControladorGalaxia cg, ControladorRuta cr, ControladorPlaneta cp, ControladorNave cn) throws Exception
+    public Entrada(ArrayList<ArrayList<Pair<Arco, Integer>>> aap) throws Exception
     {
-    	g = new Grafo(cg.convierteRutasYPlanetas(cr, cp,cn));
+    	g = new Grafo(aap);
     }
     
     //Pre: Cierto
