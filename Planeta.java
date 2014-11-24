b0d0ced7741b3fa8f3936b3bf640f83f0fe74165
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-24 12:18:14 +0100

    Update Planeta.java

--- Planeta.java
+++ Planeta.java
@@ -20,8 +20,8 @@ public class Planeta
     // Post: Crea un planeta con nombrePlaneta = n , Coste = k, Coordenadas = Coo, F indica si es Fuente y S si es Sumidero.
     public Planeta(String n, int k, Pair<Integer,Integer> Coo) throws Exception
     {
-        if(!alfa_numeric(n)) throw new Exception ("Error: El nombre de un Planeta tiene que ser alfanumerico\n");
-    	if (k < 0) throw new Exception ("Error: El Coste no puede ser negativo\n");
+        if(!alfa_numeric(n)) throw new Exception("Error: El nombre de un Planeta tiene que ser alfanumerico y contener menos de 20 caracteres");
+    	if (k < 0) throw new Exception ("Error: El Coste no puede ser negativo");
         nombrePlaneta = n;
         Coste = k;
         Coordenadas = new Pair<Integer, Integer> (Coo.consultarPrimero(), Coo.consultarSegundo());
@@ -61,7 +61,7 @@ public class Planeta
     //Post: Modifica el coste Coste, Coste = k.
     public void Modificar_Coste(int k) throws Exception
     {
-        if (k < 0) throw new Exception("Error: El Coste del Planeta no puede ser negativo\n");
+        if (k < 0) throw new Exception("Error: El Coste del Planeta no puede ser negativo");
         Coste = k;
     }
     
@@ -69,9 +69,9 @@ public class Planeta
     //Post: Modifica las Coordenadas. 
     public void modificarCoordenades(int rndX, int rndY) throws Exception
     {
-    	if(rndX < 0 || rndY < 0) throw new Exception ("Error: Las Coordenadas no pueden ser negativas\n");
-    		Coordenadas.ponPrimero(rndX);
-    		Coordenadas.ponSegundo(rndY);
+    	if(rndX < 0 || rndY < 0) throw new Exception ("Error: Las Coordenadas no pueden ser negativas");
+    	Coordenadas.ponPrimero(rndX);
+    	Coordenadas.ponSegundo(rndY);
     }
 }
 
