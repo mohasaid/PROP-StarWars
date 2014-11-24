cc65b3a6943a3d1164c9d0ed6f34693407b14fb8
elenaouro <elenaouro@gmail.com>
2014-11-24 13:14:38 +0100

    Update TipoNave2.java

--- TipoNave2.java
+++ TipoNave2.java
@@ -1,21 +1,19 @@
-import java.util.*;
+import java.util.Comparator;
 
-//TIPO2
+
+//TIPO 2:
 
 public class TipoNave2 extends Nave{
-	public static int consumo;
-	public static boolean definido = false;
+	private static int consumo=0;
+	private static boolean definido = false;
 
-	public static boolean ErrorTipografico(int i) {  
-        	return (i<=0);
-	} 
 	//pre:cierto
-		//post: el tipo de nave 2 queda definido pasando a tener consumo c asignado
-		/**
-		 * metodo que define el tipo de nave 2 asignandole un consumo
-		 * @param c
-		 * @throws Exception
-		 */
+	//post: el tipo de nave 2 queda definido pasando a tener consumo c asignado
+	/**
+	 * metodo que define el tipo de nave 2 asignandole un consumo
+	 * @param c
+	 * @throws Exception
+	 */
 	public static void DefinirTipo(int c) throws Exception{
 		if(ErrorTipografico(c)){
 			throw new Exception("Error: el consumo debe ser mayor que 0\n");
@@ -24,30 +22,7 @@ public class TipoNave2 extends Nave{
 			definido = true;
 	}
 	//pre: cierto
-	//post: crea una nave de tipo 2 vacía
-	/**
-	 * metodo que crea una nave de tipo 1 vacía
-	 */
-	public TipoNave2(){
-		ident = -1;
-		destino =-1;
-		origen = -1;
-	}
-	//pre:cierto;
-	//Post: Crea una nave de tipo 2 a partir de una nave sin tipo
-	/**
-	 * metodo que crea una nave de tipo 2 a partir de una nave sin tipo
-	 * @param n
-	 * @throws Exception
-	 */
-	public TipoNave2(Nave n) throws Exception{
-		ident = n.consultar_id();
-		destino = n.consultar_destino();
-		origen = n.consultar_origen();
-	}
-
-	//pre: cierto
-	//post: crea una nave de tipo 1 inicializada
+	//post: crea una nave de tipo 2 inicializada
 	/**
 	 * metodo que crea una nave de tipo 2 con identificador, destino y origen
 	 * @param id
@@ -55,15 +30,15 @@ public class TipoNave2 extends Nave{
 	 * @param o
 	 * @throws Exception
 	 */
-	public TipoNave2(int id,int d,int o) throws Exception{
+	public TipoNave2(int id,String d,String o) throws Exception{
 		if(ErrorTipograficoID(id)){
 			throw new Exception("Error: El identificador del tipo debe ser mayor o igual que 0\n");
 		}
-		if(ErrorTipograficoID(d)){
-			throw new Exception("Error: El identificador del planeta destino tiene que ser mayor o igual que 0\n");
+		if(!alfa_numeric(d)){
+			throw new Exception("Error: El identificador del planeta destino ha de ser alfa numerico\n");	
 		}
-		if(ErrorTipograficoID(o)){
-			throw new Exception("Error: El identificador del planeta origen tiene que ser mayor o igual que 0\n");
+		if(!alfa_numeric(o)){
+			throw new Exception("Error: El identificador del planeta destino ha de ser alfa numerico\n");	
 		}
 		if(!definido){
 			throw new Exception("Error: el tipo de nave 2 no ha sido definido\n");
@@ -72,36 +47,31 @@ public class TipoNave2 extends Nave{
 		destino = d;
 		origen = o;
 	}
-	
 	//Pre:cierto
-		//Post devuelve el estado del tipo de nave.
+	//Post devuelve el estado del tipo de nave.
 	/**
 	 * metodo que crea consulta si el tipo 2 está definido
 	 */
-		public boolean EstaDefinido(){
-			return definido;
-		}
-		
+	public static boolean EstaDefinido(){
+		return definido;
+	}
 	//pre: cierto
 	//post: retorna el nombre que identifica al tipo
-		/**
-		 * metodo que consulta el tipo de una nave
-		 * @throws Exception
-		 */
-	public int consultar_tipo() throws Exception{
+	/**
+	 * metodo que consulta el tipo de una nave
+	 * @throws Exception
+	 */
+	public int consultar_tipo(){
 		return 2;
 	}
-	
+
 	//pre: cierto
 	//post: retorna el consumo asociado al tipo
 	/**
 	 * metodo que consulta el consumo asociado al tipo de nave 2
 	 * @throws Exception
 	 */
-	public int consultar_consumo() throws Exception{
-		if(consumo == 0){
-		throw new Exception("Error: El tipo de nave no tiene un consumo asociado\n");	
-	}
+	public static int consultar_consumo(){
 		return consumo;
 	}
 
@@ -119,15 +89,5 @@ public class TipoNave2 extends Nave{
 		consumo = c;
 	}
 }
-class OrdenTipoNave2 implements Comparator<TipoNave2>{
-    public int compare(TipoNave2 n1, TipoNave2 n2){
-        try{
-            if(n2.consultar_id() < n1.consultar_id()) return 1;
-            else return -1;
-        }
-        catch(Exception e){
-            System.out.print(e);
-        }
-        return 0;
-    }
-}
+
+
