a9fc84a2aa17da21acc08a86f5b95ddbdb7169d1
elenaouro <elenaouro@gmail.com>
2014-11-24 14:54:27 +0100

    Update ControladorDadesNave.java

--- ControladorDadesNave.java
+++ ControladorDadesNave.java
@@ -4,49 +4,33 @@ import java.util.Scanner;
 
 public class ControladorDadesNave extends ControladorDades{
  
-    private boolean inicial;
- 
     //CONSTRUCTORA 
-    public ControladorDadesNave() {}
- 
-    //MODIFICADORA
-    public void Inicial(boolean b) {
-        inicial = b;
-    }
+    public ControladorDadesNave(){}
+
     //METODES
-   public String cargar(String path) throws FileNotFoundException 
-    {     
+      public String cargar(String path, int i,BufferedReader br) throws Exception 
+    {    
+	
         String res = "";
-        FileReader fr = new FileReader(path);
-        Scanner in = new Scanner(fr);
-        while (in.hasNextLine()) {
-            res += in.nextLine();
-            res += "#";
-        }
-        in.close();
+	String aux = "";
+	aux = br.readLine();
+        while ((aux!=null) && i>0){
+		res+=aux+"#";
+		--i;
+		aux = br.readLine();
+	}
         return res;
     }
      
-    public void guardar(String path, String res)throws IOException  
+     public void guardar(String path, String res)throws IOException  
     {        
-        String sep = File.separator;
-         
         PrintWriter out;
-         
-        if (inicial) {
-            out = new PrintWriter(path);
-        }
-        else {
-            out = new PrintWriter(new BufferedWriter(new FileWriter(path, true)));
-        }
-        Scanner scan = new Scanner(res);
-        scan.useDelimiter("#");
-        String s;
-        while(scan.hasNext()) {
-            s = scan.next();
-            out.println(s);            
-        }
-        scan.close();
+        out = new PrintWriter(new BufferedWriter(new FileWriter(path, true)));
+        BufferedWriter bw = new BufferedWriter(out);
+       
+        bw.write(res);
+ 
+        bw.close();
         out.close();        
-    }    
+    }
 }
