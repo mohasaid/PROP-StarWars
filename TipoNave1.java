7335b2a89922fba51664228c20573b67a7f2dbdc
elenaouro <elenaouro@gmail.com>
2014-11-24 13:13:52 +0100

    Update TipoNave1.java

--- TipoNave1.java
+++ TipoNave1.java
@@ -1,16 +1,16 @@
-import java.util.*;
+import java.util.Comparator;
 
 
-//TIPO 1:
+//TIPO 2:
 
-public class TipoNave1 extends Nave{
-	private static int consumo;
+public class TipoNave2 extends Nave{
+	private static int consumo=0;
 	private static boolean definido = false;
 
 	//pre:cierto
-	//post: el tipo de nave 1 queda definido pasando a tener consumo c asignado
+	//post: el tipo de nave 2 queda definido pasando a tener consumo c asignado
 	/**
-	 * metodo que define el tipo de nave 1 asignandole un consumo
+	 * metodo que define el tipo de nave 2 asignandole un consumo
 	 * @param c
 	 * @throws Exception
 	 */
@@ -21,17 +21,16 @@ public class TipoNave1 extends Nave{
 			consumo = c;
 			definido = true;
 	}
-
 	//pre: cierto
-	//post: crea una nave de tipo 1 inicializada
+	//post: crea una nave de tipo 2 inicializada
 	/**
-	 * metodo que crea una nave de tipo1 con identificador, destino y origen
+	 * metodo que crea una nave de tipo 2 con identificador, destino y origen
 	 * @param id
 	 * @param d
 	 * @param o
 	 * @throws Exception
 	 */
-	public TipoNave1(int id,String d,String o) throws Exception{
+	public TipoNave2(int id,String d,String o) throws Exception{
 		if(ErrorTipograficoID(id)){
 			throw new Exception("Error: El identificador del tipo debe ser mayor o igual que 0\n");
 		}
@@ -41,9 +40,8 @@ public class TipoNave1 extends Nave{
 		if(!alfa_numeric(o)){
 			throw new Exception("Error: El identificador del planeta destino ha de ser alfa numerico\n");	
 		}
-		
 		if(!definido){
-			throw new Exception("Error: el tipo de nave 1 no ha sido definido\n");
+			throw new Exception("Error: el tipo de nave 2 no ha sido definido\n");
 		}
 		ident = id;
 		destino = d;
@@ -52,7 +50,7 @@ public class TipoNave1 extends Nave{
 	//Pre:cierto
 	//Post devuelve el estado del tipo de nave.
 	/**
-	 * metodo que crea consulta si el tipo 1 está definido
+	 * metodo que crea consulta si el tipo 2 está definido
 	 */
 	public static boolean EstaDefinido(){
 		return definido;
@@ -64,13 +62,13 @@ public class TipoNave1 extends Nave{
 	 * @throws Exception
 	 */
 	public int consultar_tipo(){
-		return 1;
+		return 2;
 	}
 
 	//pre: cierto
 	//post: retorna el consumo asociado al tipo
 	/**
-	 * metodo que consulta el consumo asociado al tipo de nave 1
+	 * metodo que consulta el consumo asociado al tipo de nave 2
 	 * @throws Exception
 	 */
 	public static int consultar_consumo(){
@@ -80,7 +78,7 @@ public class TipoNave1 extends Nave{
 	//pre: cierto
 	//post: modifica el consumo asociado al tipo de nave
 	/**
-	 * metodo que modifica el consumo asociado al tipo de nave 1
+	 * metodo que modifica el consumo asociado al tipo de nave 2
 	 * @param c
 	 * @throws Exception
 	 */
@@ -91,8 +89,5 @@ public class TipoNave1 extends Nave{
 		consumo = c;
 	}
 }
-
-
-
 
 
