1e6ebf1d20a5e61247f2d2943ca9ad8b6bbb82b5
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-24 15:04:54 +0100

    Update ControladorDadesPlaneta.java

--- ControladorDadesPlaneta.java
+++ ControladorDadesPlaneta.java
@@ -1,37 +1,35 @@
 import java.io.*;
-import java.util.Scanner;
+
 
 public class ControladorDadesPlaneta extends ControladorDades {
-	private boolean inicial;
 	
-	public ControladorDadesPlaneta(){}
-	
-	public void Inicial(boolean b){
-		inicial = b;
-	}
-	public String cargar (String path) throws FileNotFoundException {
-		String res= "";
-		FileReader f = new FileReader(path);
-		Scanner sc = new Scanner(f);
-		while (sc.hasNext()){
-			res += sc.nextLine();
-			res += "#";
-		}
-		sc.close();
-		return res;
-	}
-	public void guardar(String path, String res) throws IOException{
-		PrintWriter out;
-		if(inicial) out = new PrintWriter(path);
-		else out = new PrintWriter(new BufferedWriter (new FileWriter(path, true)));
-		Scanner sc = new Scanner(res);
-		sc.useDelimiter("#");
-		String s;
-		while(sc.hasNext()) {
-			s = sc.next();
-			out.println(s);
-		}
-		sc.close();
-		out.close();
-	}
+	 //CONSTRUCTORA
+	public ControladorDadesPlaneta() {}
+
+    //METODES
+    public String cargar(String path, int i,BufferedReader br) throws Exception 
+    {    
+    	String res = "";
+    	String aux = "";
+    	aux = br.readLine();
+        while ((aux!=null) && i>0){
+        	res+=aux+"@";
+        	--i;
+        	aux = br.readLine();
+        }
+        return res;
+    }
+     
+    public void guardar(String path, String res)throws IOException  
+    {        
+        PrintWriter out;
+        out = new PrintWriter(new BufferedWriter(new FileWriter(path, true)));
+        BufferedWriter bw = new BufferedWriter(out);
+       
+        bw.write(res);
+ 
+        bw.close();
+        out.close();        
+    }
+
 }
