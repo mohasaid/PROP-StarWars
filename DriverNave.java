6b2f9462d9bea8361f39740c40c6ac656c81a1db
elenaouro <elenaouro@gmail.com>
2014-11-25 21:23:54 +0100

    Update DriverNave.java

--- DriverNave.java
+++ DriverNave.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 
 
 public class DriverNave{
+	 private static boolean creado = false;
+	 private static TipoNave1 n;
     public void Executa(Scanner sc) {
         int op;
-        TipoNave1 n = new TipoNave1();
+       
 
         
                 		System.out.print("Opciones: \n\n"
@@ -17,42 +19,40 @@ public class DriverNave{
              +"-                                                                               -\n"
              +"-   0   Salir del DriverNave                                                    -\n"
              +"-   1   TestDefinirTipo1(int consumo)                                            -\n"
-             +"-   2   TestCrearNaveTipo1(int idNave, int idDestino, int idOrigen)             -\n" 
+             +"-   2   TestCrearNaveTipo1(int idDestino, int idOrigen)                          -\n" 
              +"-   3   TestConsultarID()                                                       -\n" 
-             +"-   5   TestConsultarDestino()                                                  -\n" 
-             +"-   6   TestConsultarOrigen()                                                   -\n" 
-             +"-   7   TestConsultarIDTipo()                                                   -\n"
+             +"-   4   TestConsultarDestino()                                                  -\n" 
+             +"-   5   TestConsultarOrigen()                                                   -\n" 
+             +"-   6   TestConsultarIDTipo()                                                   -\n"
              +"-   7   TestConsultarConsumo()                                                  -\n"
-             +"-   8   TestConsultarNave()                                                     -\n" 
-             +"-   9   TestModificarID(int NuevoID)                                            -\n" 
-             +"-   10  TestModificarDestino(int NuevoDestino)                                  -\n" 
-             +"-   11  TestModificarOrigen(int NuevoOrigen)                                    -\n" 
-             +"-   12  TestModificarConsumo(int NuevoConsumo)                                  -\n" 
+             +"-   8   TestConsultarNave()                                                     -\n"  
+             +"-   9  TestModificarDestino(int NuevoDestino)                                  -\n" 
+             +"-   10  TestModificarOrigen(int NuevoOrigen)                                    -\n" 
+             +"-   11  TestModificarConsumo(int NuevoConsumo)                                  -\n" 
              +"---------------------------------------------------------------------------------\n");
 
         op = sc.nextInt(); 
                 
         while(op != 0){
             switch (op) {
-            case 1: TestDefinirTipo1(sc,n);break;
-            case 2: TestCrearNaveTipo1(sc,n);break;
-            case 3: TestConsultarID(n);break;
-            case 4: TestConsultarDestino(n);break;
-            case 5: TestConsultarOrigen(n);break;
-            case 6: TestConsultarIDTipo(n);break;
-            case 7: TestConsultarConsumo(n);break;
-            case 8: TestConsultarNave(n);break;
-            case 9: TestModificarID(sc,n);break;
-            case 10: TestModificarDestino(sc,n);break;
-            case 11: TestModificarOrigen(sc,n);break;
-            case 12: TestModificarConsumo(sc,n);break;
+            case 1: TestDefinirTipo1(sc);break;
+            case 2: TestCrearNaveTipo1(sc);break;
+            case 3: TestConsultarID();break;
+            case 4: TestConsultarDestino();break;
+            case 5: TestConsultarOrigen();break;
+            case 6: TestConsultarIDTipo();break;
+            case 7: TestConsultarConsumo();break;
+            case 8: TestConsultarNave();break;
+            case 9: TestModificarDestino(sc);break;
+            case 10: TestModificarOrigen(sc);break;
+            case 11: TestModificarConsumo(sc);break;
             default: System.out.println("Opción incorrecta");
             }
     op = sc.nextInt(); 
     }
    }
     //Creadoras
-    public static void TestDefinirTipo1(Scanner sc, TipoNave1 n){
+    public static void TestDefinirTipo1(Scanner sc){
     	try{
 	    	while(!sc.hasNextInt()){
 	            String s = sc.nextLine();
@@ -66,26 +66,19 @@ public class DriverNave{
      }
     }
     
-    public static void TestCrearNaveTipo1(Scanner sc, TipoNave1 n){
+    public static void TestCrearNaveTipo1(Scanner sc){
     	try{
-	    	if(n.consultar_id()!=-1){
-	    		throw new Exception("Error: Ya se ha creado una nave\n");
-	    	}
+	        int id = sc.nextInt();
+	        String dest = sc.next();
+	        String orig = sc.next();
+    		if(creado){
+    			throw new Exception("Error: ya se ha creado una nave");
+    		}
 	    	while(!sc.hasNextInt()){
 	            String s = sc.nextLine();
 	            throw new Exception("Error: El identificador debe ser un entero\n");
 	        }
-	        int id = sc.nextInt();
-	        while(!sc.hasNextInt()){
-	            String s = sc.nextLine();
-	            throw new Exception("Error: El identificador del planeta destino debe ser un entero\n");
-	        }
-	        int dest = sc.nextInt();
-	        while(!sc.hasNextInt()){
-	            String s = sc.nextLine();
-	            throw new Exception("Error: El identificador del planeta origen debe ser un entero\n");
-	        }
-	        int orig = sc.nextInt();
+	        creado = true;
 	        n = new TipoNave1(id,dest,orig);
     	}
     	 catch (Exception e){
@@ -93,51 +86,69 @@ public class DriverNave{
      }
     }
     //Consultoras
-    public static void TestConsultarID(TipoNave1 n){
+    public static void TestConsultarID(){
     	try{
+    		if(!creado){
+    			throw new Exception("Error: No se ha creado ninguna nave\n");
+    		}
     		System.out.println(n.consultar_id());
     	}
     	 catch (Exception e){
              System.out.print(e);
      }
     }
-    public static void TestConsultarDestino(TipoNave1 n){
+    public static void TestConsultarDestino(){
     	try{
+    		if(!creado){
+    			throw new Exception("Error: No se ha creado ninguna nave\n");
+    		}
     		System.out.println(n.consultar_destino());
     	}
     	 catch (Exception e){
              System.out.print(e);
      }
     }
-    public static void TestConsultarOrigen(TipoNave1 n){
-    	try{
+    public static void TestConsultarOrigen(){
+    	try{	
+    		if(!creado){
+    			throw new Exception("Error: No se ha creado ninguna nave\n");
+    		}
     		System.out.println(n.consultar_origen());
     	}
     	 catch (Exception e){
              System.out.print(e);
      }
     }
-    public static void TestConsultarIDTipo(TipoNave1 n){
+    public static void TestConsultarIDTipo(){
     	try{
+    		if(!creado){
+    			throw new Exception("Error: No se ha creado ninguna nave\n");
+    		}
     		System.out.println(n.consultar_tipo());
     	}
     	 catch (Exception e){
              System.out.print(e);
     	 }
     }
-    public static void TestConsultarConsumo(TipoNave1 n){
+    public static void TestConsultarConsumo(){
     	try{
+    		if(!creado){
+    			throw new Exception("Error: No se ha creado ninguna nave\n");
+    		}
     		System.out.println(n.consultar_consumo());
     	}
     	 catch (Exception e){
              System.out.print(e);
      }
     }
-    public static void TestConsultarNave(TipoNave1 n){
+    public static void TestConsultarNave(){
     	try{
+    		if(!creado){
+    			throw new Exception("Error: No se ha creado ninguna nave\n");
+    		}
     		int id = n.consultar_id();
-	    	int dest = n.consultar_id();
-	    	int orig = n.consultar_origen();
+	    	String dest = n.consultar_destino();
+	    	String orig = n.consultar_origen();
 	    	int tipo = n.consultar_tipo();
 	    	int consumo = n.consultar_consumo();
 	    	System.out.println("id: "+id+" origen:"+orig+" destino:"+dest+" tipo:"+tipo+" consumo:"+consumo);
@@ -146,48 +157,36 @@ public class DriverNave{
              System.out.print(e);
      }
     }
-    //Modificadoras
-    public static void TestModificarID(Scanner sc, TipoNave1 n){
-    	try{
-	    	while(!sc.hasNextInt()){
-	            String s = sc.nextLine();
-	            throw new Exception("Error: El identificador debe ser un entero\n");
-	        }
-	    	int idn = sc.nextInt();
-	    	n.modificar_id(idn);
-    	}
-    	 catch (Exception e){
-             System.out.print(e);
-     }
-    }
-    public static void TestModificarDestino(Scanner sc,TipoNave1 n){
+
+    public static void TestModificarDestino(Scanner sc){
 	    try{
-    		while(!sc.hasNextInt()){
-	            String s = sc.nextLine();
-	            throw new Exception("Error: El identificador del planeta destino debe ser un entero\n");
-	        }
-	    	int idn = sc.nextInt();
+    		if(!creado){
+    			throw new Exception("Error: No se ha creado ninguna nave\n");
+    		}
+	    	String idn = sc.next();
 	    	n.modificar_destino(idn);
 	    }
 	    catch (Exception e){
             System.out.print(e);
 	    }
     }
-    public static void TestModificarOrigen(Scanner sc, TipoNave1 n){
+    public static void TestModificarOrigen(Scanner sc){
     	try{
-	    	while(!sc.hasNextInt()){
-	            String s = sc.nextLine();
-	            throw new Exception("Error: El identificador del planeta origen debe ser un entero\n");
-	        }
-	    	int idn = sc.nextInt();
+    		if(!creado){
+    			throw new Exception("Error: No se ha creado ninguna nave\n");
+    		}
+	    	String idn = sc.next();
 	    	n.modificar_origen(idn);
     	}
     	 catch (Exception e){
              System.out.print(e);
      }
     }
-    public static void TestModificarConsumo(Scanner sc,TipoNave1 n){
+    public static void TestModificarConsumo(Scanner sc){
 	    try{	
+    		if(!creado){
+    			throw new Exception("Error: No se ha creado ninguna nave\n");
+    		}
     		while(!sc.hasNextInt()){
 	            String s = sc.nextLine();
 	            throw new Exception("Error: El consumo debe ser un entero\n");
