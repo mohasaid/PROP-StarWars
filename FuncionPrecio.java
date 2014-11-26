a378ddfb50443ef791d8334b0c43d2ae1203b24d
elenaouro <elenaouro@gmail.com>
2014-11-26 14:57:58 +0100

    Update FuncionPrecio.java

--- FuncionPrecio.java
+++ FuncionPrecio.java
@@ -1,10 +1,7 @@
 import java.util.*;
 
 public class FuncionPrecio extends FuncionesCoste{
-	public FuncionPrecio(){
-		r = new Ruta();
-		p = new Planeta();
-	}
+	public FuncionPrecio(){}
 	public int CalcularCoste() throws Exception{
 		return r.consultar_distancia()*p.Consultar_Coste();
 	}
