caa4eb7fc8ba6a61ced4cc6f91cc1cf539980a1a
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-26 18:03:30 +0100

    Update Entrada.java

--- Entrada.java
+++ Entrada.java
@@ -1,13 +1,11 @@
-import java.util.ArrayList;
-
 public class Entrada {
 	private Grafo g;
 	
     //Pre: Cierto
     //Post: Crea una entrada con el grafo inicializado a partir de de la lista de nodos y otra de arcos
-    public Entrada(ArrayList<ArrayList<Pair<Arco, Integer>>> aap) throws Exception
+    public Entrada(Grafo f1) throws Exception
     {
-    	g = new Grafo(aap);
+    	g = f1;
     }
     
     //Pre: Cierto
@@ -16,4 +14,6 @@ public class Entrada {
     {
     	return g;
     }
+    
+    
 }
