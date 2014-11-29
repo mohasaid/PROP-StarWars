41d02598af00346401e58e7348e8f932ffe72f3a
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-29 01:30:49 +0100

    Update ControladorDades.java

--- ControladorDades.java
+++ ControladorDades.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 import java.io.File;
 
 public class ControladorDades {
-
+     
     FileReader fr;
     PrintWriter pw;
    
-    // CONSTRUCTOR  
     public ControladorDades() {} 
    
-    // FUNCIONES 
     public void AbrirLectura(String path) throws FileNotFoundException
     {
         fr = new FileReader(new File(path));
@@ -30,26 +28,5 @@ public class ControladorDades {
     {
         fr.close();
     }
-     
-    public String cargar() 
-    {      
-        Scanner in = new Scanner(fr); 
-        in.useDelimiter("\\$"); 
-        String res;
-        try{
-	        res = in.nextLine(); 
-	        return res;
-        }
-        catch(Exception e){
-        	return "";
-        }
-    } 
-       
-    public void guardar(String res)throws IOException   
-    {   
-        if (!res.isEmpty()) {
-            String r = res + "$";
-            pw.println(r); 
-        }
-    }
+
 }
