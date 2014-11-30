86129701313a996c1600e1579b9229828e7a5b40
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-30 18:40:03 +0100

    Update MFP.java

--- MFP.java
+++ MFP.java
@@ -8,9 +8,6 @@ public abstract class MFP {
 	public void Calcular_cuellos_botellas (Salida s, ControladorPlaneta cp, ControladorRuta cr) throws Exception
 	{
 		int V = g_residual.sizeGrafo();
-		
-		//int path[] = new int[V];
-		
 		int origen = V-2;
 		
 		boolean[] visitados = new boolean[V];
@@ -30,7 +27,6 @@ public abstract class MFP {
 				int adj = g_residual.consultarSeg(actual, i);
 				int cap = g_residual.consultarPrim(actual, i).ConsultarCapacidad();
 				if(cap != 0 && !visitados[adj]) {
-					//path[adj] = actual;
 					visitados[adj] = true;
 					q1.add(adj);
 				}
@@ -52,54 +48,74 @@ public abstract class MFP {
 		}
 	}
 
-	public void Caminos(Nave n, int consumo, boolean b){
+	public void Caminos(Nave n, int consumo, boolean b, Recorrido r, ControladorPlaneta cp, Salida s) throws Exception
+	{
 		
-		//Codigo caminos
-		/*
-		int origen = n.consultar_origen();
-		int destino = n.consultar_destino();
-		int path[] = new int[g.sizeGrafo()];
-		Arrays.fill(path,-1);
-		bfs(origen,destino,path);
+		String origen = n.consultar_origen();
+		String destino = n.consultar_destino();
+		ArrayList<String> pl = cp.consultarPlanetas();
+		int o1 = pl.indexOf(origen);
+		int d1 = pl.indexOf(destino);
 		
-		Stack<Integer> st = new Stack<Integer>();
-		while(destino != -1) {
-			st.push(destino);
-			destino = path[destino];
-		}
+		int path[] = new int[g_residual.sizeGrafo()];
+		Arrays.fill(path, -1);
 		
-		String res = "";
-		if (st.size() >= 0) res = "" + st.pop();
-		while (st.size() >= 0) // el cami es separa per una ','
-		{
-			res = ", " + st.pop();
-		}
+		boolean hay = r.Recorrido(g_residual, o1, d1, path);
 		
-		if (b)
-		{
-			res = "#"; //marca cuando hay el consumo
-			consumo = 0;
-			while(destino != -1) {
-				st.push(destino);
-				destino = path[destino];
+		String res = ""; // contiene camino con nodos
+		String planetes = ""; // contiene camino con planetas
+		int cTotal = 0;
+		if(hay) {
+			// guardo el camino de nodos
+			Stack<Integer> st = new Stack<Integer>();
+			while(d1 != -1) {
+				st.push(d1);
+				d1 = path[d1];
 			}
-			int act = origen;
-			int sig = st.pop();
-			boolean trobat = false;
+			while(st.size() > 1) {
+				res += st.pop() + ",";
+			}
+			res += st.pop();
 			
-			while (!trobat){
-				consumo += g.consultaPairUn(act, sig).consultarPrimero().ConsultarCapacidad()*g.consultaPairUn(act, sig).consultarPrimero().ConsultarCoste();
-				if (sig == destino) trobat = false;
-				if (!trobat) {
-					act = sig;
-					sig = st.pop();
-				}
-				
+			// Paso de nodos a planetas
+			Scanner scan = new Scanner(res);
+			scan.useDelimiter(",");
+			while(scan.hasNext()) {
+				String pa = scan.next();
+				Integer pai = Integer.parseInt(pa);
+				String pla1 = pl.get(pai);
+				planetes += pla1 + ",";
 			}
-			res = "" + consumo;
+			scan.close();
+			
+			// calculo consumo general
+			int a1, a2, tmp = 0; // nodos
+			Scanner scan1 = new Scanner(res);
+			scan1.useDelimiter(",");
+			boolean first = true;
+			while(scan1.hasNext()) {
+				if(first) {
+					String a_1 = scan1.next();
+					a1 = Integer.parseInt(a_1);
+					String a_2 = scan1.next();
+					a2 = Integer.parseInt(a_2);
+					int cst = g_residual.consultaPairUn(a1, a2).consultarPrimero().ConsultarCoste();
+					cTotal += (cst*consumo);
+					tmp = a2;
+					first = false;
+				}
+				else {
+					String a_11 = scan1.next();
+					int a3 = Integer.parseInt(a_11);
+					int cst1 = g_residual.consultaPairUn(tmp, a3).consultarPrimero().ConsultarCoste();
+					cTotal += (cst1*consumo);
+					tmp = a3;
+				}
+			}
+			scan1.close();
+			planetes += "#" + cTotal;
 		}
-		return res;
-		*/
+		else planetes = "Esta nave no puede desplazarse ya que no existe un camino posible entre su origen y destino";
+		s.AnadirCamino(planetes);
 	}
-	
 }
