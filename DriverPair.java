bc43acfc33edc27e2c983e119a586b136dd11312
MohaSaid <MohaSaid@users.noreply.github.com>
2014-11-30 03:01:38 +0100

    Update DriverPair.java

--- DriverPair.java
+++ DriverPair.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-// COMO NO SE DE QUE VA A METER, Y NO PUEDO PONER LISTA, PORQUE NO PODRIA LEER UNA LISTA, PONGO INT
+// Se pone como ejemplo el pair de tipo integer, integer
 
 public class DriverPair {
 	private Pair<Integer, Integer> p;
@@ -9,30 +9,32 @@ public class DriverPair {
         Scanner sc = new Scanner(System.in);
         int op;
         System.out.print(
-             "---------------------------------------------------------------------------------------- \n"
-           + "                               DRIVER PAIR                               				   \n"
-           + "---------------------------------------------------------------------------------------- \n"
-           + "- Opcion 0: Salir de la gestion del driver                                        	   \n"
-           + "- Opcion 1: Pair(int a, int b)														   \n"
-           + "- Opcion 2: ponPrimero(int elemento) 													   \n"
-           + "- Opcion 3: ponSegundo(int elemento) 													   \n"
-           + "- Opcion 4: consultaPrimero()  														   \n"
-           + "- Opcion 5: consultaSegundo() 														   \n"
-           + "- Opcion 6: toString()																   \n");
+             "------------------------------------------------ \n"
+           + "                  DRIVER PAIR     			 - \n"
+           + "------------------------------------------------ \n"
+           + "- Opcion 0: Salir de la gestion del driver       \n"
+           + "- Opcion 1: Pair(int a, int b)				   \n"
+           + "- Opcion 2: ponPrimero(int elemento) 			   \n"
+           + "- Opcion 3: ponSegundo(int elemento) 			   \n"
+           + "- Opcion 4: consultaPrimero()  				   \n"
+           + "- Opcion 5: consultaSegundo() 				   \n"
+           + "- Opcion 6: toString()						   \n"
+           + "-------------------------------------------------\n");
         op = sc.nextInt(); 
         while(op != 0){
             switch (op) {
-            	case 1: TestCreaPair(sc);break;
-            	case 2: TestponPrimero(sc);break;
-            	case 3: TestponSegundo(sc);break;
-            	case 4: TestconsultaPrimero();break;
-            	case 5: TestconsultaSegundo();break;
-            	case 6: TestConsultaPinta();break;
-            	default: System.out.println("Opción incorrecta");
+            	case 1: TestCreaPair(sc); break;
+            	case 2: TestponPrimero(sc); break;
+            	case 3: TestponSegundo(sc); break;
+            	case 4: TestconsultaPrimero(); break;
+            	case 5: TestconsultaSegundo(); break;
+            	case 6: TestConsultaPintar(); break;
+            	default: System.out.println("Opcion incorrecta");
             }
     op = sc.nextInt(); 
     }
    }
+	
 	public void TestCreaPair(Scanner sc) 
 	{
 		int a = sc.nextInt();
@@ -62,7 +64,7 @@ public class DriverPair {
 		System.out.println(p.consultarSegundo());
 	}
 	
-	public void TestConsultaPinta()
+	public void TestConsultaPintar()
 	{
 		System.out.println(p.toString());
 	}
