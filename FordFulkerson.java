f7a37e7d495a2370483fe219241167697e296dee
MohaSaid <MohaSaid@users.noreply.github.com>
2014-12-01 16:31:07 +0100

    Update FordFulkerson.java

--- FordFulkerson.java
+++ FordFulkerson.java
@@ -4,14 +4,46 @@ public class FordFulkerson extends MFP {
 	
 	public FordFulkerson(Entrada e)
 	{
-		g_residual = e.Consultar_grafo();
+		G = e.Consultar_grafo();
+		g_residual();
 	}
+	
+	public void g_residual() 
+	{
+		ArrayList<ArrayList<Pair<Arco,Integer> > > ar = new ArrayList<ArrayList<Pair<Arco,Integer> > > ();
+		for(int i = 0; i < G.sizeGrafo(); ++i) {
+			ArrayList<Pair<Arco,Integer> >  ad = G.consultarCosteDestinos(i);
+			ar.add(ad);
+		}
+		g_residual = new Grafo(ar);
+		int v;
+		for(int i = 0; i < G.sizeGrafo(); ++i) {
+			for(int j = 0; j < G.sizeGrafo(i); ++j) {
+				v = G.consultarSeg(i, j);
+				if(!G.ExisteV(v, i)) {
+					Arco a = new Arco();
+					Pair<Arco,Integer> p = new Pair<Arco,Integer>(a,i);
+					g_residual.consultarCosteDestinos(v).add(p);
+				}
+			}
+		}
+		System.out.println("GRAFO residual:");
+		for(int i = 0; i < g_residual.sizeGrafo(); ++i) {
+			System.out.println("Del vertice " + i + " sale lo siguiente:");
+			for(int j = 0; j < g_residual.sizeGrafo(i); ++j) {
+				System.out.println("Arista de capacidad = " + g_residual.consultarPrim(i, j).ConsultarCapacidad()
+								 + " y con coste = " + g_residual.consultarPrim(i, j).ConsultarCoste()
+								 + " hacia el vertice "+ g_residual.consultarSeg(i, j));
+			}
+		}
+	}
+	
 	public void Ejecutar(Recorrido r, Salida s)
 	{
 		// CONSULTAR TIEMPO
 		long timeMillis = System.currentTimeMillis();
 		
-		int size = g_residual.sizeGrafo();
+		int size = G.sizeGrafo();
 		
 		int origen = size-2; // nodo origen
 		int destino = size-1; // nodo destino
@@ -24,15 +56,15 @@ public class FordFulkerson extends MFP {
 		String camino = ""; // Contiene el camino en orden inverso
 		
 		while(r.Recorrido(g_residual,origen,destino,path)) {
-			
 			int pathflow = Integer.MAX_VALUE;
 			camino += destino;
+
 			for(v = destino; v != origen; v = path[v]) {
 				u = path[v];
-				camino += ", " + u;
+				camino += "," + u;
 				pathflow = Math.min(pathflow, g_residual.consultaPairUn(u, v).consultarPrimero().ConsultarCapacidad());
-				System.out.println("Pathflow = " + pathflow);
 			}
+			
 			camino += ", " + origen;
 			for(v = destino; v != origen; v = path[v]) {
 				u = path[v];
@@ -43,9 +75,7 @@ public class FordFulkerson extends MFP {
 				tmp1 += pathflow;
 				g_residual.ponEnVerAr(v, u, tmp1);
 			}
-			System.out.println("Pathflow1 = " + pathflow);
 			max_flow += pathflow;
-			System.out.println("MAXFLOW1 = " + max_flow);
 			s.AnadirCambio(camino);
 			camino = "";
 		}
@@ -55,5 +85,5 @@ public class FordFulkerson extends MFP {
 		long timeMillis1 = System.currentTimeMillis();
 		long f1 = timeMillis1 - timeMillis;
 		s.AnadirTiempo(f1);
-	}
+	}	
 }
