314bcf7504d63533d0cd905b06f10026de8960da
Gerard9494 <gerard9494@gmail.com>
2014-12-14 11:29:59 +0100

    Create SW_Panel.java

---
+++ SW_Panel.java
@@ -0,0 +1,95 @@
+
+import com.sun.prism.paint.Color;
+import java.awt.BasicStroke;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.SystemColor;
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gerard
+ */
+public class SW_Panel extends JPanel{
+    protected int strokeSize = 1;
+    /** Color of shadow */
+    protected java.awt.Color shadowColor = java.awt.Color.DARK_GRAY;
+    /** Sets if it drops shadow */
+    protected boolean shady = true;
+    /** Sets if it has an High Quality view */
+    protected boolean highQuality = true;
+    /** Double values for Horizontal and Vertical radius of corner arcs */
+    protected Dimension arcs = new Dimension(16, 16);
+    /** Distance between shadow border and opaque panel border */
+    protected int shadowGap = 4;
+    /** The offset of shadow.  */
+    protected int shadowOffset = 3;
+    /** The transparency value of shadow. ( 0 - 255) */
+    protected int shadowAlpha = 100;
+            /*
+    public SW_Panel() {
+
+        super();
+        setOpaque(false);
+        setBackground(SystemColor.GREEN);
+        setBounds(100,100,100,100);
+        */
+        
+        /** Stroke size. it is recommended to set it to 1 for better view */
+    
+
+	public SW_Panel() {
+            super();
+            setOpaque(false);
+	}
+
+	@Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int width = getWidth();
+        int height = getHeight();
+        int shadowGap = this.shadowGap;
+        java.awt.Color shadowColorA = new java.awt.Color(shadowColor.getRed(), 
+	shadowColor.getGreen(), shadowColor.getBlue(), shadowAlpha);
+        Graphics2D graphics = (Graphics2D) g;
+
+        //Sets antialiasing if HQ.
+        if (highQuality) {
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+			RenderingHints.VALUE_ANTIALIAS_ON);
+        }
+
+        //Draws shadow borders if any.
+        if (shady) {
+            graphics.setColor(shadowColorA);
+            graphics.fillRoundRect(
+                    shadowOffset,// X position
+                    shadowOffset,// Y position
+                    width - strokeSize - shadowOffset, // width
+                    height - strokeSize - shadowOffset, // height
+                    arcs.width, arcs.height);// arc Dimension
+        } else {
+            shadowGap = 1;
+        }
+
+        //Draws the rounded opaque panel with borders.
+        graphics.setColor(getBackground());
+        graphics.fillRoundRect(0, 0, width - shadowGap, 
+		height - shadowGap, arcs.width, arcs.height);
+        graphics.setColor(getForeground());
+        graphics.setStroke(new BasicStroke(strokeSize));
+        graphics.drawRoundRect(0, 0, width - shadowGap, 
+		height - shadowGap, arcs.width, arcs.height);
+
+        //Sets strokes to default, is better.
+        graphics.setStroke(new BasicStroke());
+    }
+}
