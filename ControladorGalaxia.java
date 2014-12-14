89df6003d217cba46692bb7fab63d2e0416b5221
MohaSaid <MohaSaid@users.noreply.github.com>
2014-12-14 21:34:29 +0100

    Update ControladorGalaxia.java

--- ControladorGalaxia.java
+++ ControladorGalaxia.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prop;
+
+/**
+ *
+ * @author Moha
+ */
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
