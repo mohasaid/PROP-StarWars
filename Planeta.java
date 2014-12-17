67c48035d2fa3aa7466a9d0706aa7202dd458daa
JoseMartinNogueira <josemn15@hotmail.com>
2014-12-17 14:47:45 +0100

    Update Planeta.java

--- Planeta.java
+++ Planeta.java
@@ -6,6 +6,11 @@ public class Planeta
     private int Coste; //Precio del combustible del planeta.
     private Pair<Integer,Integer> Coordenadas; //Coordenadas del planeta en la galaxia
     
+    /**
+     * Metodo para comprobar que un string es alfanumerico
+     * @param nom
+     * @throws Exception
+     */
     private boolean alfa_numeric(String nom)
     {
         if(nom.isEmpty() || nom == null || nom.length() > 20) return false;
@@ -16,8 +21,13 @@ public class Planeta
         return true;
     }
     
-    // Pre: Cierto.
-    // Post: Crea un planeta con nombrePlaneta = n , Coste = k, Coordenadas = Coo, F indica si es Fuente y S si es Sumidero.
+    /**
+     * Metodo para crear un planeta
+     * @param n
+     * @param k
+     * @param Coo
+     * @throws Exception
+     */
     public Planeta(String n, int k, Pair<Integer,Integer> Coo) throws Exception
     {
         if(!alfa_numeric(n)) throw new Exception("Error: El nombre de un Planeta tiene que ser alfanumerico y contener menos de 20 caracteres");
@@ -26,47 +36,63 @@ public class Planeta
         Coste = k;
         Coordenadas = new Pair<Integer, Integer> (Coo.consultarPrimero(), Coo.consultarSegundo());
     }
-    //Pre: Cierto.
-    //Post: Retorna la id del planeta
+    /**
+     * Metodo para consultar el nombre de un planeta.
+     * @throws Exception
+     */
     public String Consultar_nombre()
     {
         return nombrePlaneta;
     }
-    //Pre: Cierto.
-    //Post: Retorna el Coste del planeta.
+    /**
+     * Metodo para consultar el coste de un planeta
+     * @throws Exception
+     */
     public int Consultar_Coste() 
     {
         return Coste;
     }
-    //Pre: Cierto.
-    //Post: Retorna las Coordenadas del planeta.
+    /**
+     * Metodo para consultar las coordenadas de un planeta
+     * @throws Exception
+     */
     public Pair<Integer,Integer> consultar_coordenades()
     {
         return Coordenadas;
     }
-    //Pre: Cierto.
-    //Post: Retorna la primera coordenada del planeta.
+    /**
+     * Metodo para consultar la coordenada X de un planeta
+     * @throws Exception
+     */
     public int consultar_X() 
     {
         return Coordenadas.consultarPrimero();
     }
-    
-    //Pre: Cierto.
-    //Post: Retorna la segunda coordenada del planeta.
+    /**
+     * Metodo para consultar la coordenada Y de un planeta
+     * @throws Exception
+     */
     public int consultar_Y() 
     {
         return Coordenadas.consultarSegundo();
     }
-    //Pre: Cierto.
-    //Post: Modifica el coste Coste, Coste = k.
+    /**
+     * Metodo para modificar el coste de un planeta
+     * @param k
+     * @throws Exception
+     */
     public void Modificar_Coste(int k) throws Exception
     {
         if (k < 0) throw new Exception("Error: El Coste del Planeta no puede ser negativo");
         Coste = k;
     }
     
-    //Pre: Cierto.
-    //Post: Modifica las Coordenadas. 
+    /**
+     * Metodo para modificar las coordenadas de un planeta
+     * @param randX
+     * @param randY
+     * @throws Exception
+     */
     public void modificarCoordenades(int rndX, int rndY) throws Exception
     {
     	if(rndX < 0 || rndY < 0) throw new Exception ("Error: Las Coordenadas no pueden ser negativas");
