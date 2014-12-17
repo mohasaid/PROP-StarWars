caee3abe964da6bbd7aac6456eddd045dcc65566
MohaSaid <MohaSaid@users.noreply.github.com>
2014-12-17 22:04:31 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+package prop;
 /**
  *
  * @author Moha
@@ -13,9 +14,21 @@ public class Main {
 		                       UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
 		                     }
 		                     catch (Exception e) {}
-		                     
-		                    ControladorVistaGalaxia cVistaGalaxia = new ControladorVistaGalaxia();		                     
-		                    VistaPrincipal frame = new VistaPrincipal(cVistaGalaxia);
+		                    
+		                    ControladorVistaGalaxia cVistaGalaxia = new ControladorVistaGalaxia();
+                                    ControladorVistaNave cVistaNave = new ControladorVistaNave();
+                                    ControladorVistaRuta cVistaRuta = new ControladorVistaRuta();
+                                    ControladorVistaPlaneta cVistaPlaneta = new ControladorVistaPlaneta();
+                                    ControladorVistaMFP cVistaMFP = new  ControladorVistaMFP();
+                                    
+                                    cVistaPlaneta.ImportarVistas(cVistaGalaxia);
+                                    cVistaRuta.inicialitza(cVistaPlaneta);
+                                    cVistaNave.start(cVistaPlaneta);
+                                    cVistaMFP.Importar(cVistaGalaxia, cVistaPlaneta, cVistaRuta, cVistaNave);
+                                    
+                                    
+                                    
+		                    VistaPrincipal frame = new VistaPrincipal(cVistaGalaxia,cVistaPlaneta,cVistaNave,cVistaRuta,cVistaMFP);
 		                    frame.setVisible(true);                                
 		                }
 		                catch (Exception e) {
