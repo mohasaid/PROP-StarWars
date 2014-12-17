c8a9f82a216e47ba4f8f1c0c22645f79af531d39
MohaSaid <MohaSaid@users.noreply.github.com>
2014-12-17 22:04:08 +0100

    Update MFP.java

--- MFP.java
+++ MFP.java
@@ -1,10 +1,6 @@
 public abstract class MFP {
 	protected Grafo g_residual;
 	protected Grafo G;
-	
-	/*
-	 * GRAFO ENTRADA, GRAFO RESIDUAL EN SALIDA
-	 */
 
 	public abstract void Ejecutar(Recorrido r, Salida s);
 	
@@ -12,4 +8,5 @@ public abstract class MFP {
 	{
 		return g_residual;
 	}
+
 }
