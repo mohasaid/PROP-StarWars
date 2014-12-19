c655a156c17ee9431acbe28b59adcf767e83283f
elenaouro <elenaouro@gmail.com>
2014-12-19 01:10:24 +0100

    Update TipoNave3.java

--- TipoNave3.java
+++ TipoNave3.java
@@ -1,3 +1,4 @@
+
 import java.util.Comparator;
 
 
@@ -21,6 +22,9 @@ public class TipoNave3 extends Nave{
 			consumo = c;
 			definido = true;
 	}
+                public static void Desdefinir(){
+            definido=false;
+        }
 	//pre: cierto
 	//post: crea una nave de tipo 3 inicializada
 	/**
