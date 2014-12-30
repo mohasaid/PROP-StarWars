fe4c83d624beb50f84d2c0d84411dc04d9a15d75
MohaSaid <MohaSaid@users.noreply.github.com>
2014-12-30 21:45:03 +0100

    Create DibujoGrafo.java

---
+++ DibujoGrafo.java
@@ -0,0 +1,53 @@
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+/**
+ *
+ * @author Moha
+ */
+public class DibujoGrafo {
+    private Graph<String, String> g;
+    private static final String nodo = "N";
+    private static final String orf = "OF";
+    private static final String def = "DF";
+    
+    public DibujoGrafo(Grafo g1)
+    {
+        g = new DirectedSparseGraph<String, String>();
+        int tam = g1.sizeGrafo();
+        int of = tam - 2;
+        int df = tam - 1;
+        
+        g.addVertex(orf);
+        for(int i = 0; i < of; ++i) {
+            String res = nodo + i;
+            g.addVertex(res);
+        }
+        g.addVertex(def);
+        
+        for(int i = 0; i < tam; ++i) {
+            for(int j = 0; j < g1.sizeGrafo(i); ++j) {
+                int v = g1.consultarSeg(i, j);
+                if(i == of) {
+                    g.addEdge("f"+i, orf, nodo+v);
+                }
+                else if(v != df) {
+                    String a = i +""+ v;
+                    g.addEdge(a, nodo+i, nodo+v);
+                }
+                else {
+                    g.addEdge("f"+i, nodo+i, def);
+                }
+            }
+        }
+        for(int i = 0; i < g1.sizeGrafo(of); ++i) {
+            int v = g1.consultarSeg(of, i);
+            g.addEdge("ff"+i,orf,nodo+v);
+        }
+    }
+   
+    public Graph<String, String> consultaGrafo()
+    {
+        return g;
+    }
+                        
+}
