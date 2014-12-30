4d134941aef3d26b33c3d99bc4d33ffbfdc93b57
MohaSaid <MohaSaid@users.noreply.github.com>
2014-12-30 21:46:30 +0100

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -1,5 +1,3 @@
-package prop;
-
 /**
  *
  * @author Moha
@@ -7,8 +5,8 @@ package prop;
 import java.util.*;
 
 public class Dijkstra extends Recorrido {
-	protected int dist[];
-        private static final int arist = 2000000000;
+	protected int dist[]; // con infinito
+        private static final int arist = 2000000000; // infinito
 	
 	public boolean Recorrido(Grafo g_residual, int origen, int destino, int path[]) {
 		int V = g_residual.sizeGrafo();
@@ -17,31 +15,31 @@ public class Dijkstra extends Recorrido {
 		
 		dist = new int[V];
 		Arrays.fill(dist, arist);
+                
 		dist[origen] = 0;
 		
-		PriorityQueue<Pair<Integer, Integer> > pq = new PriorityQueue<Pair<Integer, Integer> >(1, new PriorityQueueComparator()); // coste, nodo
+		PriorityQueue<Pair<Integer, Integer> > pq = new PriorityQueue<Pair<Integer, Integer> >(0, new PriorityQueueComparator()); // coste, nodo
 		Pair<Integer, Integer> pa = new Pair<Integer, Integer>(0, origen);
 		pq.add(pa);
 		while(!pq.isEmpty()) {
-			Pair<Integer, Integer> p = pq.poll();
-			int actual = p.consultarSegundo();
-			int size = g_residual.sizeGrafo(actual);
-			if(!visitados[actual]) {
-				visitados[actual] = true;
-				for(int i = 0; i < size; ++i) {
-					int adj = g_residual.consultarSeg(actual, i);
-					int coste = g_residual.consultarPrim(actual, i).ConsultarCoste();
-					int cap = g_residual.consultarPrim(actual, i).ConsultarCapacidad();
-					if((dist[adj] > dist[actual] + coste) && cap > 0 && (coste >= 0)) {
-						dist[adj] = dist[actual] + coste;
-						path[adj] = actual;
-						pq.add(new Pair<Integer,Integer>(dist[adj],adj));
-					}
-				}
-			}
+                    Pair<Integer, Integer> p = pq.poll();
+                    int actual = p.consultarSegundo();
+                    int size = g_residual.sizeGrafo(actual);
+                    if(!visitados[actual]) {
+                            visitados[actual] = true;
+                            for(int i = 0; i < size; ++i) {
+                                    int adj = g_residual.consultarSeg(actual, i);
+                                    int coste = g_residual.consultarPrim(actual, i).ConsultarCoste();
+                                    int cap = g_residual.consultarPrim(actual, i).ConsultarCapacidad();
+                                    if((dist[adj] > dist[actual] + coste) && (cap > 0) && (coste >= 0)) {
+                                        dist[adj] = dist[actual] + coste;
+                                        path[adj] = actual;
+                                        pq.add(new Pair<Integer,Integer>(dist[adj],adj));
+                                    }
+                            }
+                    }
 		}
-		if(dist[destino] != arist) return true;
-		else return false;
+		return (dist[destino] != arist);
 	}
 }
 
