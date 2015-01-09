5634e288a1695af7015f46496e297124e5bd714b
MohaSaid <MohaSaid@users.noreply.github.com>
2015-01-09 03:13:38 +0100

    Update VistaHelp.java

--- VistaHelp.java
+++ VistaHelp.java
@@ -1,21 +1,11 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Elena
  */
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javafx.scene.control.ComboBox;
-
 
 public class VistaHelp extends PrimerNivel{
 
