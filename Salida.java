a962bec03f288fc488e66c08b95af4a40d9dc0b7
MohaSaid <MohaSaid@users.noreply.github.com>
2015-01-12 21:20:38 +0100

    Update Salida.java

--- Salida.java
+++ Salida.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prop;
+
 /**
  *
  * @author Moha
@@ -16,12 +23,12 @@ public class Salida {
 
 	public Salida() 
 	{
-		CuellosB = new ArrayList<Integer>();
-		Caminos = new ArrayList<String>();
-		Cambios = new ArrayList<String>();
-		max_flow = 0;
-		tiempo = 0;
-                algoritmo = "";
+            CuellosB = new ArrayList<Integer>();
+            Caminos = new ArrayList<String>();
+            Cambios = new ArrayList<String>();
+            max_flow = 0;
+            tiempo = 0;
+            algoritmo = "";
 	}
 
 	// CONSULTORAS
@@ -58,39 +65,44 @@ public class Salida {
 	 * @return
 	 * @throws Exception
 	 */
-	public int[] Caminos(Grafo g, int naves, String origen, String destino, boolean b, Recorrido r, ArrayList<String> pl) throws Exception
+	public int[] Caminos(Grafo g, int naves, String origen, String destino, Recorrido r, ArrayList<String> pl, boolean[] hay_s) throws Exception
 	{
-		int o1 = pl.indexOf(origen); // nodo origen
-		int d1 = pl.indexOf(destino); // nodo destino
-		
-		int tam = g.sizeGrafo();
-		int path[] = new int[tam+1]; // en la posicion tam estara la capacidad minima
-		Arrays.fill(path,-1);
-		path[tam] = 0;
-		int u,v;
-		boolean hay;
-		hay = r.Recorrido(g, o1, d1, path);
-		if(hay) {
-			for(v = d1; v != o1; v = path[v]) {
-				u = path[v];
-				path[tam] = Math.min(Integer.MAX_VALUE, g.consultaPairUn(u, v).consultarPrimero().ConsultarCapacidad());
-			}
-			if(naves >= path[tam]) naves = path[tam];
-			// paso las naves
-			for(v = d1; v != o1; v = path[v]) {
-				u = path[v];
-				int tmp = g.consultaPairUn(u, v).consultarPrimero().ConsultarCapacidad();
-				tmp = tmp - naves;
-				g.consultaPairUn(u, v).consultarPrimero().ModificarCapacidad(tmp);
-			}
-		}
-		return path;
+            int o1 = pl.indexOf(origen); // nodo origen
+            int d1 = pl.indexOf(destino); // nodo destino
+
+            int tam = g.sizeGrafo();
+
+            int path[] = new int[tam+1]; // en la posicion tam estara la capacidad minima
+            Arrays.fill(path,-1);
+
+            path[tam] = 0;
+            int u,v;
+
+            boolean hay;
+            hay = r.Recorrido(g, o1, d1, path);
+            hay_s[0] = hay;
+            
+            if(hay) {
+                for(v = d1; v != o1; v = path[v]) {
+                        u = path[v];
+                        path[tam] = Math.min(Integer.MAX_VALUE, g.consultaPairUn(u, v).consultarPrimero().ConsultarCapacidad());
+                }
+                if(naves >= path[tam]) naves = path[tam];
+                // paso las naves
+                for(v = d1; v != o1; v = path[v]) {
+                        u = path[v];
+                        int tmp = g.consultaPairUn(u, v).consultarPrimero().ConsultarCapacidad();
+                        tmp = tmp - naves;
+                        g.consultaPairUn(u, v).consultarPrimero().ModificarCapacidad(tmp);
+                }
+            }
+            return path;
 	}
 	
 	//Pre:cierto
 	//Post: devuelve la suma del numero de elementos en Flujos, y cuellosB
 	public int size(){
-            return Caminos.size()+CuellosB.size();
+            return Caminos.size() + CuellosB.size() + 1;
 	}
 	//Pre:cierto
 	//Post: devuelve el numero de elementos en Cambios
