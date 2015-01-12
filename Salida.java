6d38d3a743748fa9db6542d140274511fba84cb8
MohaSaid <MohaSaid@users.noreply.github.com>
2015-01-12 21:21:54 +0100

    Update Salida.java

--- Salida.java
+++ Salida.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package prop;
-
-/**
- *
- * @author Moha
- */
 import java.util.*;
 
 public class Salida {
